5050cf09af8c57f6e9fd8af8ee99c4db781288ce
fpo <fpo@adorsys.com>
2017-12-10 20:17:33 +0100

    initial version

---
+++ src/main/java/de/adorsys/aspsp/xs2a/spi/impl/AccountSPIImpl.java
@@ -0,0 +1,29 @@
+package de.adorsys.aspsp.xs2a.spi.impl;
+
+import java.util.Date;
+import java.util.List;
+
+import de.adorsys.aspsp.xs2a.spi.domain.Account;
+import de.adorsys.aspsp.xs2a.spi.domain.AccountReport;
+import de.adorsys.aspsp.xs2a.spi.domain.Balances;
+import de.adorsys.aspsp.xs2a.spi.service.AccountSPI;
+
+public class AccountSPIImpl implements AccountSPI {
+
+	public List<Account> readAccounts(Boolean withBalance, Boolean psuInvolved) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Balances readBalances(Boolean psuInvolved) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public AccountReport readTransactions(String accountId, Date dateFROM, Date dateTo, String transactionID,
+			Boolean psuInvolved) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
