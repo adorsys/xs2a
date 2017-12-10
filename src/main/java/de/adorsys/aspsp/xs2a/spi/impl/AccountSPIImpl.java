782268009dd9c4d3b87e8a7bbbe3d01942708d63
fpo <fpo@adorsys.com>
2017-12-10 20:40:35 +0100

    adding spring dependencies

--- src/main/java/de/adorsys/aspsp/xs2a/spi/impl/AccountSPIImpl.java
+++ src/main/java/de/adorsys/aspsp/xs2a/spi/impl/AccountSPIImpl.java
@@ -3,11 +3,14 @@ package de.adorsys.aspsp.xs2a.spi.impl;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import de.adorsys.aspsp.xs2a.spi.domain.Account;
 import de.adorsys.aspsp.xs2a.spi.domain.AccountReport;
 import de.adorsys.aspsp.xs2a.spi.domain.Balances;
 import de.adorsys.aspsp.xs2a.spi.service.AccountSPI;
 
+@Component
 public class AccountSPIImpl implements AccountSPI {
 
 	public List<Account> readAccounts(Boolean withBalance, Boolean psuInvolved) {
