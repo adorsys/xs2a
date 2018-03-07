e5766e7edf2d136ba76d106dc9bd22cb6bb224e3
roman <rye@adorsys.com.ua>
2018-03-07 12:41:50 +0100

    added comment with link to task

--- spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/Tpp.java
+++ spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/Tpp.java
@@ -6,7 +6,8 @@ import lombok.Data;
 @Data
 @JsonInclude(com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL)
 public class Tpp {
-	//TODO... I defined the Tpp attributes as string because they aren't still defined.
+	//TODO... I defined the Tpp attributes as string because they aren't still defined. https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/41
+    //
 	//This information will be in the certificate
 
 	private String tpp_provider_identification;
