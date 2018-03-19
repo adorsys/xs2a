8512f917a97797b89f303c4b34046fbcd1370348
roman <rye@adorsys.com.ua>
2018-03-19 14:26:14 +0200

    Added final type for fields instead @Setter(AccessLevel.NONE)

--- spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/PersonName.java
+++ spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/PersonName.java
@@ -2,17 +2,14 @@ package de.adorsys.aspsp.xs2a.spi.domain;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AccessLevel;
 import lombok.Data;
-import lombok.Setter;
 
 @Data
-@Setter(AccessLevel.NONE)
 @ApiModel(description = "Person name", value = "Michael, Schmidt")
 public class PersonName {
     @ApiModelProperty(value = "First name", example = "Michael")
-    private String firstName;
+    private final String firstName;
     
     @ApiModelProperty(value = "Last name", example = "Schmidt")
-    private String lastName;
+    private final String lastName;
 }
--- spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/address/CountryCode.java
+++ spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/address/CountryCode.java
@@ -2,17 +2,12 @@ package de.adorsys.aspsp.xs2a.spi.domain.address;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Setter;
 
 @Data
-@Setter(AccessLevel.NONE)
-@AllArgsConstructor
 @ApiModel(description = "Country code", value = "49")
 public class CountryCode {
     
     @ApiModelProperty(value = "Country code", required = true, example = "49")
-    private String code;
+    private final String code;
 }
