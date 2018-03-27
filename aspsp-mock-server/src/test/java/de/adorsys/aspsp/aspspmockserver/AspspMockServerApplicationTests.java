fe9462f87a1251f4f0db6b1c66582f3cc985fd50
Denys Golubiev <dgo@adorsys.de>
2018-03-27 07:53:43 +0200

    Add spring-boot mock-server module

---
+++ aspsp-mock-server/src/test/java/de/adorsys/aspsp/aspspmockserver/AspspMockServerApplicationTests.java
@@ -0,0 +1,16 @@
+package de.adorsys.aspsp.aspspmockserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AspspMockServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
