5b25390701b5b4b57cfb8aa8a36538a9953e76a2
may <mpo@adorsys.com.ua>
2018-03-28 16:23:39 +0300

    added AccountMapperTest

--- xs2a-impl/src/test/java/de/adorsys/aspsp/xs2a/service/ConsentMapperTest.java
+++ xs2a-impl/src/test/java/de/adorsys/aspsp/xs2a/service/ConsentMapperTest.java
@@ -1,8 +1,11 @@
 package de.adorsys.aspsp.xs2a.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.Gson;
+import de.adorsys.aspsp.xs2a.domain.Balances;
 import de.adorsys.aspsp.xs2a.domain.TransactionStatus;
 import de.adorsys.aspsp.xs2a.domain.ais.consent.AccountConsent;
+import de.adorsys.aspsp.xs2a.domain.ais.consent.ConsentStatus;
 import de.adorsys.aspsp.xs2a.domain.ais.consent.CreateConsentReq;
 import de.adorsys.aspsp.xs2a.spi.domain.account.SpiAccountConsent;
 import de.adorsys.aspsp.xs2a.spi.domain.common.SpiTransactionStatus;
@@ -17,13 +20,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ConsentMapperTest {
     private final String CREATE_CONSENT_REQ_JSON_PATH = "/json/CreateAccountConsentReqTest.json";
-    private final String ACCOUNT_CONSENT_REQ_JSON_PATH = "/json/MapGetAccountConsentTest.json";
+    private final String SPI_ACCOUNT_CONSENT_REQ_JSON_PATH = "/json/MapGetAccountConsentTest.json";
     private final Charset UTF_8 = Charset.forName("utf-8");
 
     @Autowired
@@ -58,15 +62,27 @@ public class ConsentMapperTest {
     @Test
     public void mapGetAccountConsent() throws IOException{
         //Given:
-        String aicRequestJson = IOUtils.resourceToString(ACCOUNT_CONSENT_REQ_JSON_PATH, UTF_8);
-        SpiAccountConsent donorConsent = new Gson().fromJson(aicRequestJson, SpiAccountConsent.class);
-        AccountConsent expectedConsent = new Gson().fromJson(aicRequestJson, AccountConsent.class);
+        String accountConsentJson = IOUtils.resourceToString(SPI_ACCOUNT_CONSENT_REQ_JSON_PATH, UTF_8);
+        SpiAccountConsent donorAccountConsent = new Gson().fromJson(accountConsentJson, SpiAccountConsent.class);
 
         //When:
-        AccountConsent actualConsent = consentMapper.mapGetAccountConsent(donorConsent);
+        assertNotNull(donorAccountConsent);
+        AccountConsent actualAccountConsent = consentMapper.mapGetAccountConsent(donorAccountConsent);
 
         //Then:
-        assertThat(actualConsent).isEqualTo(expectedConsent);
+        assertThat(actualAccountConsent.getId()).isEqualTo("3dc3d5b3-7023-4848-9853-f5400a64e80f");
+        assertThat(actualAccountConsent.getAccess().getBalances()[0].getIban()).isEqualTo("DE2310010010123456789");
+        assertThat(actualAccountConsent.getAccess().getBalances()[1].getIban()).isEqualTo("DE2310010010123456790");
+        assertThat(actualAccountConsent.getAccess().getBalances()[1].getCurrency().getCurrencyCode()).isEqualTo("USD");
+        assertThat(actualAccountConsent.getAccess().getBalances()[2].getIban()).isEqualTo("DE2310010010123456788");
+        assertThat(actualAccountConsent.getAccess().getTransactions()[0].getIban()).isEqualTo("DE2310010010123456789");
+        assertThat(actualAccountConsent.getAccess().getTransactions()[1].getMaskedPan()).isEqualTo("123456xxxxxx1234");
+        assertThat(actualAccountConsent.isRecurringIndicator()).isTrue();
+        assertThat(actualAccountConsent.getValidUntil()).isEqualTo("2017-11-01");
+        assertThat(actualAccountConsent.getFrequencyPerDay()).isEqualTo(4);
+        assertThat(actualAccountConsent.getLastActionDate()).isEqualTo("2017-11-01");
+        assertThat(actualAccountConsent.getTransactionStatus()).isEqualTo(TransactionStatus.ACCP);
+        assertThat(actualAccountConsent.getConsentStatus()).isEqualTo(ConsentStatus.VALID);
     }
 
 
