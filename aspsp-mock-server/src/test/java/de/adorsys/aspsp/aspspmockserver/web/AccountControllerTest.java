ddef9bfe21a293061d6aa92cb306bf65e3c84ebc
may <mpo@adorsys.com.ua>
2018-04-04 10:11:20 +0300

    added AccountServiceTest for ASPSP mock server

---
+++ aspsp-mock-server/src/test/java/de/adorsys/aspsp/aspspmockserver/web/AccountControllerTest.java
@@ -0,0 +1,20 @@
+package de.adorsys.aspsp.aspspmockserver.web;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountControllerTest {
+
+    @Test
+    public void readAllAccounts() {
+    }
+
+    @Test
+    public void readAllAccounts1() {
+    }
+
+    @Test
+    public void createAccount() {
+    }
+}
