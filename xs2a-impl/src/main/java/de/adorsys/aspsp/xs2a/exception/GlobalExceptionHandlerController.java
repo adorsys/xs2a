035e7cd3b0c397b4f1ff67fe68bd1dba77d7745d
Peter <pru@adorsys.com.ua>
2018-04-05 17:58:47 +0100

    Removed unnecessary lombok annotation

--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/exception/GlobalExceptionHandlerController.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/exception/GlobalExceptionHandlerController.java
@@ -1,6 +1,5 @@
 package de.adorsys.aspsp.xs2a.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.HandlerMethod;
 
 @Slf4j
-@AllArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
 
