d9e0b3de72bf506a375473eab884e6417017327e
Peter <pru@adorsys.com.ua>
2018-04-05 17:37:13 +0100

    Removed unnecessary spring property, fixed global exception handler

--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/exception/GlobalExceptionHandlerController.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/exception/GlobalExceptionHandlerController.java
@@ -1,6 +1,5 @@
 package de.adorsys.aspsp.xs2a.exception;
 
-import de.adorsys.aspsp.xs2a.service.MessageService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,13 +12,12 @@ import org.springframework.web.method.HandlerMethod;
 @AllArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandlerController {
-    private MessageService messageService;
 
     @ExceptionHandler(value = {Exception.class})
     public ResponseEntity handleTppException(Exception ex, HandlerMethod handlerMethod) {
         log.warn("Uncatched exception handled in Controller: {}, message: {}", handlerMethod.getMethod().getDeclaringClass().getSimpleName(), ex.getMessage());
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
-        return new ResponseEntity(messageService.getMessage(status.name()), status);
+        return new ResponseEntity(status.getReasonPhrase(), status);
     }
 }
