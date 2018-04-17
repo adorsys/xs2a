b9875dacd58b75fc295f111b6d305aef63c39dee
may <mpo@adorsys.com.ua>
2018-04-17 18:06:00 +0300

    changed coding style

--- aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/service/PaymentService.java
+++ aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/service/PaymentService.java
@@ -3,29 +3,16 @@ package de.adorsys.aspsp.aspspmockserver.service;
 import de.adorsys.aspsp.aspspmockserver.repository.PaymentRepository;
 import de.adorsys.aspsp.xs2a.spi.domain.payment.SpiSinglePayments;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
+import javax.validation.constraints.NotNull;
 
 @AllArgsConstructor
 @Service
 public class PaymentService {
     private final PaymentRepository paymentRepository;
 
-    public ResponseEntity<SpiSinglePayments> addPayment(SpiSinglePayments payment) {
-        if (payment != null) {
-            payment.setPaymentId(generatePaymentId());
-            SpiSinglePayments saved = paymentRepository.save(payment);
-            if (saved != null) {
-                return new ResponseEntity<>(saved, HttpStatus.CREATED);
-            }
-        }
-        return ResponseEntity.badRequest().build();
-    }
-
-    private static String generatePaymentId() {
-        return UUID.randomUUID().toString();
+    public SpiSinglePayments addPayment(@NotNull SpiSinglePayments payment) {
+        return paymentRepository.save(payment);
     }
 }
--- aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/web/PaymentController.java
+++ aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/web/PaymentController.java
@@ -3,6 +3,7 @@ package de.adorsys.aspsp.aspspmockserver.web;
 import de.adorsys.aspsp.aspspmockserver.service.PaymentService;
 import de.adorsys.aspsp.xs2a.spi.domain.payment.SpiSinglePayments;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +19,11 @@ public class PaymentController {
 
     @PostMapping(path = "/")
     public ResponseEntity<SpiSinglePayments> createPayment(
-                                        @RequestBody SpiSinglePayments payment) throws Exception {
-        return paymentService.addPayment(payment);
+    @RequestBody SpiSinglePayments payment) throws Exception {
+        SpiSinglePayments saved = paymentService.addPayment(payment);
+        if (saved != null) {
+            return new ResponseEntity<>(saved, HttpStatus.CREATED);
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
