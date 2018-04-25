6831ef4dd4d6474a689953c41bbcaf3a3d0b4049
Peter <pru@adorsys.com.ua>
2018-04-25 17:43:07 +0100

    changed BearerToken on RequiredArgConstructor lombok annotation

--- spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/security/BearerToken.java
+++ spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/security/BearerToken.java
@@ -1,10 +1,10 @@
 package de.adorsys.aspsp.xs2a.spi.domain.security;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BearerToken {
     private final String token;
 }
