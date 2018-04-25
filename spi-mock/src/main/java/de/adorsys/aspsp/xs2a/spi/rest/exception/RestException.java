cab142c998fe3ebb3110062d2a9e147a28bb9e88
Peter <pru@adorsys.com.ua>
2018-04-25 18:05:10 +0100

    small fix

--- spi-mock/src/main/java/de/adorsys/aspsp/xs2a/spi/rest/exception/RestException.java
+++ spi-mock/src/main/java/de/adorsys/aspsp/xs2a/spi/rest/exception/RestException.java
@@ -1,17 +1,10 @@
 package de.adorsys.aspsp.xs2a.spi.rest.exception;
 
-import lombok.Getter;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
 
 @Value
-@Getter
 public class RestException extends RuntimeException {
     private HttpStatus httpStatus;
     private String message;
-
-    public RestException(HttpStatus httpStatus, String message) {
-        this.httpStatus = httpStatus;
-        this.message = message;
-    }
 }
