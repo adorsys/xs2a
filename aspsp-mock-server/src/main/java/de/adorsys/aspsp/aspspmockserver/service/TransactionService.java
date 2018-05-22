fc0e296a64f8e5f0942b5827260040b010e6c489
Dmitry Mishchuk <dmi@adorsys.com.ua>
2018-05-23 00:40:34 +0300

    Implemented transactions in aspsp-mock

---
+++ aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/service/TransactionService.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.aspsp.aspspmockserver.service;
+
+import de.adorsys.aspsp.aspspmockserver.repository.TransactionRepository;
+import de.adorsys.aspsp.xs2a.spi.domain.account.SpiTransaction;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Currency;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class TransactionService {
+    private final TransactionRepository transactionRepository;
+
+    public List<SpiTransaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Optional<SpiTransaction> getTransactionById(String transactionId) {
+        return Optional.ofNullable(transactionRepository.findOne(transactionId));
+    }
+
+    public Optional<String> saveTransaction(SpiTransaction transaction) {
+        return Optional.ofNullable(transactionRepository.save(transaction))
+                   .map(SpiTransaction::getTransactionId);
+    }
+
+    public List<SpiTransaction> getTransactionsByAccountId(String iban, Currency currency, Date dateFrom, Date dateTo) {
+        return transactionRepository.findAllByDates(iban, currency, dateFrom, dateTo);
+    }
+
+}
---
+++ aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/web/TransactionController.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.aspsp.aspspmockserver.web;
+
+import de.adorsys.aspsp.aspspmockserver.service.TransactionService;
+import de.adorsys.aspsp.xs2a.spi.domain.account.SpiTransaction;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.AuthorizationScope;
+import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Currency;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.util.CollectionUtils.isEmpty;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/transactions")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    @ApiOperation(value = "", authorizations = {@Authorization(value = "oauth2", scopes = {@AuthorizationScope(scope = "read", description = "Access read API")})})
+    @GetMapping(path = "/")
+    public ResponseEntity<List<SpiTransaction>> readAllTransactions() {
+        return Optional.of(transactionService.getAllTransactions())
+                   .map(ResponseEntity::ok)
+                   .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @ApiOperation(value = "", authorizations = {@Authorization(value = "oauth2", scopes = {@AuthorizationScope(scope = "read", description = "Access read API")})})
+    @GetMapping(path = "/{transactionId}")
+    public ResponseEntity<SpiTransaction> readTransactionById(@PathVariable("transactionId") String transactionId) {
+        return transactionService.getTransactionById(transactionId)
+                   .map(ResponseEntity::ok)
+                   .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @ApiOperation(value = "", authorizations = {@Authorization(value = "oauth2", scopes = {@AuthorizationScope(scope = "read", description = "Access read API")})})
+    @PostMapping(path = "/")
+    public ResponseEntity createTransaction(@RequestBody SpiTransaction transaction) {
+        return transactionService.saveTransaction(transaction)
+                   .map(transactionId -> new ResponseEntity<>(transactionId, CREATED))
+                   .orElse(ResponseEntity.badRequest().build());
+    }
+
+    @ApiOperation(value = "", authorizations = {@Authorization(value = "oauth2", scopes = {@AuthorizationScope(scope = "read", description = "Access read API")})})
+    @GetMapping(path = "/{iban}/{currency}/{dateFrom}/{dateTo}")
+    public ResponseEntity<List<SpiTransaction>> readTransactionsByDates(@PathVariable("iban") String iban, @PathVariable("currency") Currency currency,
+                                                                        @PathVariable("dateFrom") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateFrom,
+                                                                        @PathVariable("dateTo") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date dateTo) {
+        List<SpiTransaction> response = transactionService.getTransactionsByAccountId(iban, currency, dateFrom, dateTo);
+        return isEmpty(response)
+                   ? ResponseEntity.notFound().build()
+                   : ResponseEntity.ok(response);
+    }
+}
