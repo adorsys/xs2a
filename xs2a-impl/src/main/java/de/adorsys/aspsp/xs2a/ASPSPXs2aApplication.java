a6563305cdc018d4e637ec0667ce013e15dcd7c9
Guy Moyo <guymoyo@yahoo.fr>
2018-06-21 12:32:22 +0200

    refactor rolefilter

--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/ASPSPXs2aApplication.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/ASPSPXs2aApplication.java
@@ -18,9 +18,7 @@ package de.adorsys.aspsp.xs2a;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 
-@ServletComponentScan
 @SpringBootApplication
 public class ASPSPXs2aApplication {
 
