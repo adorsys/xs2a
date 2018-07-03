0a2cb13432e5670047ea9b7441c88669de72b329
Guy Moyo <guymoyo@yahoo.fr>
2018-07-03 11:02:22 +0200

    pmd rule

--- psd2-validator/src/main/java/de/adorsys/psd2/validator/common/RoleOfPSP.java
+++ psd2-validator/src/main/java/de/adorsys/psd2/validator/common/RoleOfPSP.java
@@ -30,11 +30,12 @@ public class RoleOfPSP {
 		sequence = new DERSequence(new ASN1Encodable[] { roleOfPspOid, roleOfPspName });
 	}
 
+	//NOPMD
 	public static RoleOfPSP getInstance(ASN1Encodable asn1Encodable) {
 		ASN1Sequence sequence = ASN1Sequence.getInstance(asn1Encodable);
 		ASN1ObjectIdentifier objectIdentifier = ASN1ObjectIdentifier.getInstance(sequence.getObjectAt(0));
 		DERUTF8String instance = DERUTF8String.getInstance(sequence.getObjectAt(1));
-		//NOPMD
+		
 		if (RoleOfPspOid.id_psd2_role_psp_as.getId().equals(objectIdentifier.getId())
 				&& RoleOfPspName.PSP_AS.getString().equals(instance.getString())) {
 			return PSP_AS;
