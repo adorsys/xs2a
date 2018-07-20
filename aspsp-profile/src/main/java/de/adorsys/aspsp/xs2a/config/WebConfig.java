46901b022d173885a9aa9fb937487dfac8462b86
roman <elroman4@gmail.com>
2018-07-20 16:55:41 +0200

    reformatted bank_profile.yml

--- aspsp-profile/src/main/java/de/adorsys/aspsp/xs2a/config/WebConfig.java
+++ aspsp-profile/src/main/java/de/adorsys/aspsp/xs2a/config/WebConfig.java
@@ -28,7 +28,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
