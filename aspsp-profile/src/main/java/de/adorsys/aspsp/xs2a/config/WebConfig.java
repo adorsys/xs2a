5ba35daa11a99850a075cd0b3f2f366d392de75e
cga <cga@adorsys.de>
2018-07-21 19:04:25 +0200

    remove unuse import

--- aspsp-profile/src/main/java/de/adorsys/aspsp/xs2a/config/WebConfig.java
+++ aspsp-profile/src/main/java/de/adorsys/aspsp/xs2a/config/WebConfig.java
@@ -25,7 +25,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 @Configuration
