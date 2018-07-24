55c95903482b5dcf85c560406788dfe9f825ff97
may <mpo@adorsys.com.ua>
2018-07-24 09:56:05 +0200

    added BalanceType and SpiBalanceType

---
+++ spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/account/SpiBalanceType.java
@@ -0,0 +1,10 @@
+package de.adorsys.aspsp.xs2a.spi.domain.account;
+
+public enum SpiBalanceType {
+    CLOSING_BOOKED,
+    EXPECTED,
+    AUTHORISED,
+    OPENING_BOOKED,
+    INTERIM_AVAILABLE,
+    FORWARD_AVAILABLE
+}
---
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/domain/BalanceType.java
@@ -0,0 +1,10 @@
+package de.adorsys.aspsp.xs2a.domain;
+
+public enum BalanceType {
+    CLOSING_BOOKED,
+    EXPECTED,
+    AUTHORISED,
+    OPENING_BOOKED,
+    INTERIM_AVAILABLE,
+    FORWARD_AVAILABLE
+}
