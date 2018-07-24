e9932dbebfe2e34ebdfbe8f6be7063ed1dbc3f34
Mariia Polikarpova <mpo@adorsys.com.ua>
2018-07-24 11:57:03 +0000

    Update SpiTransactionStatus.java

--- spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/common/SpiTransactionStatus.java
+++ spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/common/SpiTransactionStatus.java
@@ -22,7 +22,7 @@ public enum SpiTransactionStatus {
     ACSC("AcceptedSettlementCompleted"),  //Settlement on the debtor's account has been completed. Usage : this can be used by the first agent to report to the debtor that the transaction has been completed. Warning : this status is provided for transaction status reasons, not for financial information. It can only be used after bilateral agreement"),
     ACSP("AcceptedSettlementInProcess"),  //All preceding checks such as technical validation and customer profile were successful and therefore the payment initiation has been accepted for execution
     ACTC("AcceptedTechnicalValidation"),  //AuthenticationObject and syntactical and semantical validation are successful"),
-    ACWC("AcceptedWithChange"),  //Instruction is accepted but a change will be made, such as referenceDate or remittance not sent
+    ACWC("AcceptedWithChange"),  //Instruction is accepted but a change will be made, such as date or remittance not sent
     ACWP("AcceptedWithoutPosting"),  //Payment instruction included in the credit transfer is accepted without being posted to the creditor customer’s account
     RCVD("Received"),  //Payment initiation has been received by the receiving agent
     PDNG("Pending"),  //Payment initiation or individual transaction included in the payment initiation is pending. Further checks and status update will be performed
