c9e13d1746d9cf09d4ac20b6ba6192abce5d330f
Andreas Winter <andreas.winter.ex@teambank.de>
2018-07-26 12:00:34 +0200

    Delete unused import

--- aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/web/view/PaymentConfirmationController.java
+++ aspsp-mock-server/src/main/java/de/adorsys/aspsp/aspspmockserver/web/view/PaymentConfirmationController.java
@@ -29,7 +29,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
