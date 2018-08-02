292d4e43e26ca6ae676949fa2b5e78349c4ba934
Denys Golubiev <dgo@adorsys.de>
2018-08-02 20:42:02 +0200

    Equals uses byte-array field as an object to compare

--- spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/consent/AspspConsentData.java
+++ spi-api/src/main/java/de/adorsys/aspsp/xs2a/spi/domain/consent/AspspConsentData.java
@@ -2,7 +2,28 @@ package de.adorsys.aspsp.xs2a.spi.domain.consent;
 
 import lombok.Value;
 
+import java.util.Arrays;
+
 @Value
 public class AspspConsentData {
     private byte[] aspspConsentData;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        AspspConsentData that = (AspspConsentData) o;
+
+        return Arrays.equals(aspspConsentData, that.aspspConsentData);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(aspspConsentData);
+    }
 }
