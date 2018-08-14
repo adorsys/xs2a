abd4affd0b260781e0d8524ed0843b4b249911b4
Simon Schmoll <ssc@adorsys.de>
2018-08-14 15:58:55 +0200

    new classes for creating consents in postgres db

---
+++ integration-tests/src/main/java/de/adorsys/aspsp/xs2a/integtest/config/rest/consent/AccountMapper.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.aspsp.xs2a.integtest.config.rest.consent;
+
+import de.adorsys.aspsp.xs2a.domain.*;
+import de.adorsys.aspsp.xs2a.domain.account.AccountDetails;
+import de.adorsys.aspsp.xs2a.domain.account.AccountReference;
+import de.adorsys.aspsp.xs2a.domain.account.AccountReport;
+import de.adorsys.aspsp.xs2a.domain.code.BankTransactionCode;
+import de.adorsys.aspsp.xs2a.domain.code.PurposeCode;
+import de.adorsys.aspsp.xs2a.spi.domain.account.*;
+import de.adorsys.aspsp.xs2a.spi.domain.common.SpiAmount;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Component
+public class AccountMapper {
+    public AccountDetails mapToAccountDetails(SpiAccountDetails accountDetails) {
+        return Optional.ofNullable(accountDetails)
+                   .map(ad -> new AccountDetails(
+                           ad.getId(),
+                           ad.getIban(),
+                           ad.getBban(),
+                           ad.getPan(),
+                           ad.getMaskedPan(),
+                           ad.getMsisdn(),
+                           ad.getCurrency(),
+                           ad.getName(),
+                           ad.getAccountType(),
+                           mapToAccountType(ad.getCashSpiAccountType()),
+                           ad.getBic(),
+                           mapToBalancesList(ad.getBalances())
+                       )
+                   )
+                   .orElse(null);
+    }
+
+    public Amount mapToAmount(SpiAmount spiAmount) {
+        return Optional.ofNullable(spiAmount)
+                   .map(a -> {
+                       Amount amount = new Amount();
+                       amount.setContent(a.getContent().toString());
+                       amount.setCurrency(a.getCurrency());
+                       return amount;
+                   })
+                   .orElse(null);
+    }
+
+    public Optional<AccountReport> mapToAccountReport(List<SpiTransaction> spiTransactions) {
+
+        if (spiTransactions.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Transactions[] booked = spiTransactions
+                                    .stream()
+                                    .filter(transaction -> transaction.getBookingDate() != null)
+                                    .map(this::mapToTransaction)
+                                    .toArray(Transactions[]::new);
+
+        Transactions[] pending = spiTransactions
+                                     .stream()
+                                     .filter(transaction -> transaction.getBookingDate() == null)
+                                     .map(this::mapToTransaction)
+                                     .toArray(Transactions[]::new);
+
+        return Optional.of(new AccountReport(booked, pending));
+    }
+
+    public AccountReference mapToAccountReference(SpiAccountReference spiAccountReference) {
+        return Optional.ofNullable(spiAccountReference)
+                   .map(ar -> getAccountReference(ar.getIban(), ar.getBban(), ar.getPan(), ar.getMaskedPan(), ar.getMsisdn(), ar.getCurrency()))
+                   .orElse(null);
+
+    }
+
+    public List<SpiAccountReference> mapToSpiAccountReferences(List<AccountReference> references) {
+        return Optional.ofNullable(references)
+                   .map(ref -> ref.stream()
+                                   .map(this::mapToSpiAccountReference)
+                                   .collect(Collectors.toList()))
+                   .orElse(Collections.emptyList());
+    }
+
+    public SpiAccountReference mapToSpiAccountReference(AccountReference account) {
+        return Optional.ofNullable(account)
+                   .map(ac -> new SpiAccountReference(
+                       ac.getIban(),
+                       ac.getBban(),
+                       ac.getPan(),
+                       ac.getMaskedPan(),
+                       ac.getMsisdn(),
+                       ac.getCurrency()))
+                   .orElse(null);
+    }
+
+    public List<AccountReference> mapToAccountReferences(List<SpiAccountReference> references) {
+        return Optional.ofNullable(references)
+                   .map(ref -> ref.stream()
+                                   .map(this::mapToAccountReference)
+                                   .collect(Collectors.toList()))
+                   .orElse(Collections.emptyList());
+    }
+
+    private Transactions mapToTransaction(SpiTransaction spiTransaction) {
+        return Optional.ofNullable(spiTransaction)
+                   .map(t -> {
+                       Transactions transactions = new Transactions();
+                       transactions.setAmount(mapToAmount(t.getSpiAmount()));
+                       transactions.setBankTransactionCodeCode(new BankTransactionCode(t.getBankTransactionCodeCode()));
+                       transactions.setBookingDate(t.getBookingDate());
+                       transactions.setValueDate(t.getValueDate());
+                       transactions.setCreditorAccount(mapToAccountReference(t.getCreditorAccount()));
+                       transactions.setDebtorAccount(mapToAccountReference(t.getDebtorAccount()));
+                       transactions.setCreditorId(t.getCreditorId());
+                       transactions.setCreditorName(t.getCreditorName());
+                       transactions.setUltimateCreditor(t.getUltimateCreditor());
+                       transactions.setDebtorName(t.getDebtorName());
+                       transactions.setUltimateDebtor(t.getUltimateDebtor());
+                       transactions.setEndToEndId(t.getEndToEndId());
+                       transactions.setMandateId(t.getMandateId());
+                       transactions.setPurposeCode(new PurposeCode(t.getPurposeCode()));
+                       transactions.setTransactionId(t.getTransactionId());
+                       transactions.setRemittanceInformationStructured(t.getRemittanceInformationStructured());
+                       transactions.setRemittanceInformationUnstructured(t.getRemittanceInformationUnstructured());
+                       return transactions;
+                   })
+                   .orElse(null);
+    }
+
+    public List<AccountReference> mapToAccountReferencesFromDetails(List<SpiAccountDetails> details) {
+        return Optional.ofNullable(details)
+                   .map(det -> det.stream()
+                                   .map(this::mapToAccountDetails)
+                                   .map(this::mapToAccountReference)
+                                   .collect(Collectors.toList()))
+                   .orElse(Collections.emptyList());
+    }
+
+    private List<Balance> mapToBalancesList(List<SpiAccountBalance> spiBalances) {
+        if (CollectionUtils.isEmpty(spiBalances)) {
+            return new ArrayList<>();
+        }
+
+        return spiBalances.stream()
+                   .map(this::mapToBalance)
+                   .collect(Collectors.toList());
+    }
+
+    private AccountReference mapToAccountReference(AccountDetails details) {
+        return Optional.ofNullable(details)
+                   .map(det-> getAccountReference(det.getIban(), det.getBban(), det.getPan(), det.getMaskedPan(), det.getMsisdn(), det.getCurrency()))
+                   .orElse(null);
+
+    }
+
+    private AccountReference getAccountReference(String iban, String bban, String pan, String maskedPan, String msisdn, Currency currency) {
+        AccountReference reference = new AccountReference();
+        reference.setIban(iban);
+        reference.setBban(bban);
+        reference.setPan(pan);
+        reference.setMaskedPan(maskedPan);
+        reference.setMsisdn(msisdn);
+        reference.setCurrency(currency);
+        return reference;
+    }
+
+    private CashAccountType mapToAccountType(SpiAccountType spiAccountType) {
+        return Optional.ofNullable(spiAccountType)
+                   .map(type -> CashAccountType.valueOf(type.name()))
+                   .orElse(null);
+    }
+
+    private Balance mapToBalance(SpiAccountBalance spiAccountBalance) {
+        return Optional.ofNullable(spiAccountBalance)
+                   .map(b -> {
+                       Balance balance = new Balance();
+                       balance.setBalanceAmount(mapToAmount(spiAccountBalance.getSpiBalanceAmount()));
+                       balance.setBalanceType(BalanceType.valueOf(spiAccountBalance.getSpiBalanceType().name()));
+                       balance.setLastChangeDateTime(spiAccountBalance.getLastChangeDateTime());
+                       balance.setReferenceDate(spiAccountBalance.getReferenceDate());
+                       balance.setLastCommittedTransaction(spiAccountBalance.getLastCommittedTransaction());
+                       return balance;
+                   })
+                   .orElse(null);
+    }
+}
---
+++ integration-tests/src/main/java/de/adorsys/aspsp/xs2a/integtest/utils/ConsentTestData.java
@@ -0,0 +1,72 @@
+package de.adorsys.aspsp.xs2a.integtest.utils;
+
+import de.adorsys.aspsp.xs2a.domain.account.AccountReference;
+import de.adorsys.aspsp.xs2a.domain.consent.AccountAccess;
+import de.adorsys.aspsp.xs2a.domain.consent.CreateConsentReq;
+import de.adorsys.aspsp.xs2a.integtest.config.rest.consent.AisConsentRemoteUrls;
+import de.adorsys.aspsp.xs2a.integtest.config.rest.consent.ConsentMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Currency;
+import java.util.List;
+
+@Component
+public class ConsentTestData {
+
+
+    @Qualifier("consentRestTemplate")
+    private final RestTemplate consentRestTemplate;
+    private final AisConsentRemoteUrls remoteAisConsentUrls;
+    private final ConsentMapper consentMapper;
+
+    @Autowired
+    public ConsentTestData(RestTemplate consentRestTemplate, AisConsentRemoteUrls remoteAisConsentUrls, ConsentMapper consentMapper){
+        this.consentRestTemplate = consentRestTemplate;
+        this.remoteAisConsentUrls = remoteAisConsentUrls;
+        this.consentMapper = consentMapper;
+    }
+
+    private CreateConsentReq getConsentReq(int tppFrequency, LocalDate expire, boolean recurring, AccountAccess accountAccess) {
+            CreateConsentReq consent = new CreateConsentReq();
+
+            consent.setFrequencyPerDay(tppFrequency);
+            consent.setValidUntil(expire);
+            consent.setAccess(accountAccess);
+            consent.setRecurringIndicator(recurring);
+            consent.setCombinedServiceIndicator(false);
+            return consent;
+        }
+
+    private String createConsent(CreateConsentReq request, String psuId, String tppId) {
+        return consentRestTemplate.postForEntity(remoteAisConsentUrls.createAisConsent(), consentMapper.mapToAisConsentRequest(request, psuId, tppId), String.class).getBody();
+    }
+
+
+    private AccountAccess createAccountAccessTestData(List<AccountReference> accounts, List<AccountReference> balances, List<AccountReference> transactions){
+        AccountAccess accountAccess = new AccountAccess(accounts, balances, transactions, null, null);
+        return accountAccess;
+    }
+
+    private List<AccountReference> createAccountReferenceListTestData () {
+        AccountReference accountReference1 = new AccountReference();
+        accountReference1.setIban("DE52500105173911841934");
+        accountReference1.setCurrency(Currency.getInstance("EUR"));
+
+        return Collections.singletonList(accountReference1);
+    }
+
+    public String createConsentTestData () {
+        List <AccountReference> accounts = createAccountReferenceListTestData();
+        List <AccountReference> balances = createAccountReferenceListTestData();
+        List <AccountReference> transactions = createAccountReferenceListTestData();
+        AccountAccess accountAccess = createAccountAccessTestData(accounts, balances, transactions);
+        CreateConsentReq consentReq = getConsentReq(1, LocalDate.parse("2020-11-10"),false, accountAccess);
+        return createConsent(consentReq,"d9e71419-24e4-4c5a-8d93-fcc23153aaff", "tpp01" );
+    }
+}
+
