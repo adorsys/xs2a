1639d565ff9f1b5ca2d3d31d08658bfa185bab33
Jana Tiimus <jti@adorsys.de>
2018-08-15 11:54:58 +0200

    add change consent status method to ConsentTestData

--- integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/util/ConsentTestData.java
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/util/ConsentTestData.java
@@ -1,15 +1,33 @@
-package de.adorsys.aspsp.xs2a.integtest.utils;
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.aspsp.xs2a.integtest.util;
 
 import de.adorsys.aspsp.xs2a.domain.account.AccountReference;
 import de.adorsys.aspsp.xs2a.domain.consent.AccountAccess;
 import de.adorsys.aspsp.xs2a.domain.consent.CreateConsentReq;
 import de.adorsys.aspsp.xs2a.integtest.config.rest.consent.AisConsentRemoteUrls;
 import de.adorsys.aspsp.xs2a.integtest.config.rest.consent.ConsentMapper;
+import de.adorsys.aspsp.xs2a.spi.domain.consent.SpiConsentStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Currency;
@@ -67,6 +85,10 @@ public class ConsentTestData {
         AccountAccess accountAccess = createAccountAccessTestData(accounts, balances, transactions);
         CreateConsentReq consentReq = getConsentReq(1, LocalDate.parse("2020-11-10"),false, accountAccess);
         return createConsent(consentReq,"d9e71419-24e4-4c5a-8d93-fcc23153aaff", "tpp01" );
+    }
+
+    public void changeAccountConsentStatus (String consentId, SpiConsentStatus consentStatus) {
+        consentRestTemplate.put(remoteAisConsentUrls.updateAisConsentStatus(), null, consentId, consentStatus.name());
     }
 }
 
