0f89a5d0ea36e53e5ad19d67c5ec49abd63a5a84
Sebastian Wild <swi@adorsys.de>
2018-08-15 14:38:27 +0200

    add periodic payment tests from other branch

---
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/entities/ITPeriodicPayments.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.aspsp.xs2a.integtest.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import de.adorsys.aspsp.xs2a.domain.pis.SinglePayment;
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.util.Optional;
+
+
+@Data
+public class ITPeriodicPayments extends SinglePayment {
+
+    private LocalDate startDate;
+
+    private String executionRule;
+
+    private LocalDate endDate;
+
+    private String frequency;
+
+    @Max(31)
+    @Min(1)
+    private int dayOfExecution; //Day here max 31
+
+    @JsonIgnore
+    public boolean isValidDate() {
+        return isValidDate() && isValidStartDate() //TODO Should be removed with https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/167
+                   &&
+                   Optional.ofNullable(this.endDate)
+                       .map(d -> d.isAfter(this.startDate))
+                       .orElse(true);
+    }
+
+    @JsonIgnore
+    private boolean isValidStartDate() { //TODO Should be removed with https://git.adorsys.de/adorsys/xs2a/aspsp-xs2a/issues/167
+        return this.startDate.isEqual(ChronoLocalDate.from(LocalDate.now()))
+                   || this.startDate.isAfter(ChronoLocalDate.from(LocalDate.now()));
+    }
+}
---
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/pis/PeriodicPaymentSteps.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.aspsp.xs2a.integtest.stepdefinitions.pis;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import de.adorsys.aspsp.xs2a.domain.pis.PaymentInitialisationResponse;
+import de.adorsys.aspsp.xs2a.integtest.entities.ITMessageError;
+import de.adorsys.aspsp.xs2a.integtest.entities.ITPeriodicPayments;
+import de.adorsys.aspsp.xs2a.integtest.model.TestData;
+import de.adorsys.aspsp.xs2a.integtest.util.Context;
+import de.adorsys.aspsp.xs2a.integtest.util.PaymentUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+
+
+@FeatureFileSteps
+public class PeriodicPaymentSteps {
+
+    @Autowired
+    @Qualifier("xs2a")
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private Context<ITPeriodicPayments, HashMap, PaymentInitialisationResponse> context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @And("^PSU wants to initiate a recurring payment (.*) using the payment product (.*)$")
+    public void loadTestDataForPeriodicPayment(String fileName, String paymentProduct) throws IOException {
+        context.setPaymentProduct(paymentProduct);
+        File periodicPaymentJsonFile = new File("src/test/resources/data-input/pis/recurring/" + fileName);
+
+        TestData<ITPeriodicPayments, HashMap> data = objectMapper.readValue(periodicPaymentJsonFile, new TypeReference<TestData<ITPeriodicPayments, HashMap>>() {
+        });
+
+        context.setTestData(data);
+
+        //TODO to be solved in a good manner
+        context.getTestData().getRequest().getBody().setEndDate(LocalDate.now().plusDays(100));
+    }
+
+    @When("^PSU sends the recurring payment initiating request$")
+    public void sendPeriodicPaymentInitiatingRequest() {
+        HttpEntity<ITPeriodicPayments> entity = PaymentUtils.getPaymentsHttpEntity(context.getTestData().getRequest(), context.getAccessToken());
+
+        ResponseEntity<PaymentInitialisationResponse> responseEntity = restTemplate.exchange(
+            context.getBaseUrl() + "/periodic-payments/" + context.getPaymentProduct(),
+            HttpMethod.POST,
+            entity,
+            new ParameterizedTypeReference<PaymentInitialisationResponse>() {
+            });
+
+        context.setActualResponse(responseEntity);
+    }
+
+    @Then("^a successful response code and the appropriate recurring payment response data")
+    public void checkResponseCodeFromPeriodicPayment() {
+        Map responseBody = context.getTestData().getResponse().getBody();
+        ResponseEntity<PaymentInitialisationResponse> responseEntity = context.getActualResponse();
+        HttpStatus expectedStatus = context.getTestData().getResponse().getHttpStatus();
+        assertThat(responseEntity.getStatusCode(), equalTo(expectedStatus));
+        assertThat(responseEntity.getBody().getTransactionStatus().name(), equalTo(responseBody.get("transactionStatus")));
+        assertThat(responseEntity.getBody().getPaymentId(), notNullValue());
+    }
+
+    @When("^PSU sends the recurring payment initiating request with error$")
+    public void sendFalsePeriodicPaymentInitiatingRequest() throws IOException {
+        HttpEntity<ITPeriodicPayments> entity = PaymentUtils.getPaymentsHttpEntity(context.getTestData().getRequest(), context.getAccessToken());
+
+        try {
+            restTemplate.exchange(
+                context.getBaseUrl() + "/periodic-payments/" + context.getPaymentProduct(),
+                HttpMethod.POST,
+                entity,
+                new ParameterizedTypeReference<PaymentInitialisationResponse>() {
+                });
+
+        } catch (HttpClientErrorException hce) {
+            ResponseEntity<PaymentInitialisationResponse> actualResponse = new ResponseEntity<>(
+                hce.getStatusCode());
+            context.setActualResponse(actualResponse);
+
+            ITMessageError messageError = objectMapper.readValue(hce.getResponseBodyAsString(), ITMessageError.class);
+            context.setMessageError(messageError);
+        }
+    }
+
+    /*
+     * @Then("^an error response code is displayed the appropriate error response$")
+     * see SinglePaymentSteps.java
+     */
+}
