390765532ca53ff3c0dc120596ddcd448aa16d1c
Guy Moyo <guymoyo@yahoo.fr>
2018-08-17 11:39:25 +0200

    qwac certificate filter mock

--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/config/SecurityConfig.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package de.adorsys.aspsp.xs2a.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,15 +15,18 @@ import de.adorsys.psd2.validator.certificate.util.TppRole;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    @Autowired
+    QwacCertificateFilter qwacCertificateFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().authorizeRequests().antMatchers("/swagger-ui.html", "/swagger-resources/**", "/v2/api-docs", "/webjars/**", "/swagger.json")
-		.permitAll().antMatchers("/api/v1/accounts/**").hasRole(TppRole.AISP.name()).antMatchers("/api/v1/consents/**")
-		.hasRole(TppRole.AISP.name()).antMatchers("/api/v1/funds-confirmations/**").hasRole(TppRole.PIISP.name())
-		.antMatchers("/api/v1/bulk-payments/**").hasRole(TppRole.PISP.name()).antMatchers("/api/v1/payments/**")
-		.hasRole(TppRole.PISP.name()).antMatchers("/api/v1/periodic-payments/**").hasRole(TppRole.PISP.name())
-		.anyRequest().authenticated()
-		.and().addFilterBefore(new QwacCertificateFilter(), BasicAuthenticationFilter.class);
-	}
+            .permitAll().antMatchers("/api/v1/accounts/**").hasRole(TppRole.AISP.name()).antMatchers("/api/v1/consents/**")
+            .hasRole(TppRole.AISP.name()).antMatchers("/api/v1/funds-confirmations/**").hasRole(TppRole.PIISP.name())
+            .antMatchers("/api/v1/bulk-payments/**").hasRole(TppRole.PISP.name()).antMatchers("/api/v1/payments/**")
+            .hasRole(TppRole.PISP.name()).antMatchers("/api/v1/periodic-payments/**").hasRole(TppRole.PISP.name())
+            .anyRequest().authenticated()
+            .and().addFilterBefore(qwacCertificateFilter, BasicAuthenticationFilter.class);
+    }
 }
--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/web/filter/QwacCertificateFilter.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/web/filter/QwacCertificateFilter.java
@@ -16,13 +16,19 @@
 
 package de.adorsys.aspsp.xs2a.web.filter;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import de.adorsys.psd2.validator.certificate.util.CertificateExtractorUtil;
+import de.adorsys.psd2.validator.certificate.util.TppCertificateData;
+import lombok.extern.slf4j.Slf4j;
+import no.difi.certvalidator.api.CertificateValidationException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -30,73 +36,71 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.filter.GenericFilterBean;
-
-import de.adorsys.psd2.validator.certificate.util.CertificateExtractorUtil;
-import de.adorsys.psd2.validator.certificate.util.TppCertificateData;
-import lombok.extern.slf4j.Slf4j;
-import no.difi.certvalidator.api.CertificateValidationException;
-
+@Profile("default")
+@Component
 @Slf4j
 public class QwacCertificateFilter extends GenericFilterBean {
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-			throws IOException, ServletException {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
 
-		if (!(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse)) {
-			throw new ServletException("OncePerRequestFilter just supports HTTP requests");
-		}
+        if (!(request instanceof HttpServletRequest) || !(response instanceof HttpServletResponse)) {
+            throw new ServletException("OncePerRequestFilter just supports HTTP requests");
+        }
 
-		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		if (Objects.isNull(authentication)) {
-			HttpServletRequest httpRequest = (HttpServletRequest) request;
-			String encodedTppQwacCert = httpRequest.getHeader("tpp-qwac-certificate");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (Objects.isNull(authentication)) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String encodedTppQwacCert = getEncodedTppQwacCert(httpRequest);
 
-			if (StringUtils.isNotBlank(encodedTppQwacCert)) {
-				TppCertificateData tppCertificateData;
-				try {
-					tppCertificateData = CertificateExtractorUtil.extract(encodedTppQwacCert);
+            if (StringUtils.isNotBlank(encodedTppQwacCert)) {
+                TppCertificateData tppCertificateData;
+                try {
+                    tppCertificateData = CertificateExtractorUtil.extract(encodedTppQwacCert);
 
-					HashMap<String, String> credential = new HashMap<>();
-					credential.put("autorithyCountry", tppCertificateData.getPspAuthorityCountry());
-					credential.put("autorithyId", tppCertificateData.getPspAuthorityId());
-					credential.put("autorithyName", tppCertificateData.getPspAuthorityName());
-					credential.put("autorizationNumber", tppCertificateData.getPspAuthorizationNumber());
-					credential.put("name", tppCertificateData.getPspName());
+                    HashMap<String, String> credential = new HashMap<>();
+                    credential.put("autorithyCountry", tppCertificateData.getPspAuthorityCountry());
+                    credential.put("autorithyId", tppCertificateData.getPspAuthorityId());
+                    credential.put("autorithyName", tppCertificateData.getPspAuthorityName());
+                    credential.put("autorizationNumber", tppCertificateData.getPspAuthorizationNumber());
+                    credential.put("name", tppCertificateData.getPspName());
 
-					List<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+                    List<GrantedAuthority> authorities;
 
-					authorities = tppCertificateData.getPspRoles().stream()
-							.map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
-							.collect(Collectors.toList());
+                    authorities = tppCertificateData.getPspRoles().stream()
+                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                        .collect(Collectors.toList());
 
-					authentication = new UsernamePasswordAuthenticationToken(
-							tppCertificateData.getPspAuthorizationNumber(), credential, authorities);
+                    authentication = new UsernamePasswordAuthenticationToken(
+                        tppCertificateData.getPspAuthorizationNumber(), credential, authorities);
 
-					SecurityContextHolder.getContext().setAuthentication(authentication);
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
 
-				} catch (CertificateValidationException e) {
-					log.debug(e.getMessage());
-					((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
-				}
+                } catch (CertificateValidationException e) {
+                    log.debug(e.getMessage());
+                    ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+                }
 
-			}
-		}
+            }
+        }
 
-		chain.doFilter(request, response);
+        chain.doFilter(request, response);
 
-	}
+    }
 
-	@Override
-	public void destroy() {
-	}
+    @Override
+    public void destroy() {
+    }
+
+    public String getEncodedTppQwacCert(HttpServletRequest httpRequest) {
+        return httpRequest.getHeader("tpp-qwac-certificate");
+    }
 
 }
