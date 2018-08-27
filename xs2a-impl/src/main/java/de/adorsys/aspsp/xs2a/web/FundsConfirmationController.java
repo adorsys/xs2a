fb33274a6e010a448a913e330f63df3815f7fa41
ihorkuchevskiy <iku@adorsys.com.ua>
2018-08-27 15:52:28 +0300

    Qwac certificate: - add "required = true" for "tpp-qwac-certificate” annotation to all methods in controllers xs2a - add "tppQwacCertificate" to CommonRequestHeader - delete unnecessary @ApiModelProperty in CommonRequestHeader

--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/web/FundsConfirmationController.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/web/FundsConfirmationController.java
@@ -54,7 +54,7 @@ public class FundsConfirmationController {
         @ApiImplicitParam(name = "digest", value = "730f75dafd73e047b86acb2dbd74e75dcb93272fa084a9082848f2341aa1abb6", dataType = "String", paramType = "header"),
         @ApiImplicitParam(name = "signature", value = "98c0", dataType = "String", paramType = "header"),
         @ApiImplicitParam(name = "tpp-signature-certificate", value = "some certificate", dataType = "String", paramType = "header"),
-        @ApiImplicitParam(name = "tpp-qwac-certificate", value = "qwac certificate", dataType = "String", paramType = "header")})
+        @ApiImplicitParam(name = "tpp-qwac-certificate", value = "qwac certificate", required = true, dataType = "String", paramType = "header")})
     public ResponseEntity<FundsConfirmationResponse> fundConfirmation(@RequestBody @Valid FundsConfirmationRequest request) {
         ResponseObject accountReferenceValidationResponse = referenceValidationService.validateAccountReferences(request.getAccountReferences());
         ResponseObject<FundsConfirmationResponse> response = accountReferenceValidationResponse.hasError()
