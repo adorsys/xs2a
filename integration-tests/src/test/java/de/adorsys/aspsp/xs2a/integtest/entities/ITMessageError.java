5a7ecb7fa35266d80656f0ec3b026fea3b2ac430
Hergy Tchuinkou <htc@adorsys.de>
2018-08-29 10:02:42 +0200

    refractor tppmessage error text in periodic payment and unused import

--- integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/entities/ITMessageError.java
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/entities/ITMessageError.java
@@ -2,12 +2,9 @@ package de.adorsys.aspsp.xs2a.integtest.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import de.adorsys.psd2.model.TppMessageGeneric;
 import de.adorsys.psd2.model.TppMessages;
 import de.adorsys.psd2.model.TransactionStatus;
 import lombok.Data;
-
-import java.util.Set;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
