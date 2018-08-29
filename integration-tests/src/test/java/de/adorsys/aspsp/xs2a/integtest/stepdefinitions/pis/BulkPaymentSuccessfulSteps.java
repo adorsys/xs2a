c65ed6ca455cda84d6d2f0a53bb0ea601f856c82
Simon Schmoll <ssc@adorsys.de>
2018-08-29 14:18:26 +0200

    added the payment initiation (todo was resolved), changed if statement to assertThat

--- integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/pis/BulkPaymentSuccessfulSteps.java
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/pis/BulkPaymentSuccessfulSteps.java
@@ -21,12 +21,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import de.adorsys.aspsp.xs2a.integtest.model.TestData;
 import de.adorsys.aspsp.xs2a.integtest.util.Context;
 import de.adorsys.psd2.model.BulkPaymentInitiationSctJson;
 import de.adorsys.psd2.model.PaymentInitationRequestResponse201;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -63,15 +68,15 @@ public class BulkPaymentSuccessfulSteps {
         context.setTestData(data);
     }
 
-    //TODO Uncomment after ISO DateTime format rework
-    /*@When("^PSU sends the bulk payment initiating request$")
+
+    @When("^PSU sends the bulk payment initiating request$")
     public void sendBulkPaymentInitiatingRequest() {
         HttpHeaders headers = new HttpHeaders();
         headers.setAll(context.getTestData().getRequest().getHeader());
         headers.add("Authorization", "Bearer " + context.getAccessToken());
         headers.add("Content-Type", "application/json");
 
-        List<BulkPaymentInitiationSctJson> paymentsList = context.getTestData().getRequest().getBody();
+        BulkPaymentInitiationSctJson paymentsList = context.getTestData().getRequest().getBody();
 
         ResponseEntity<List<PaymentInitationRequestResponse201>> response = restTemplate.exchange(
             context.getBaseUrl() + "/bulk-payments/" + context.getPaymentProduct(),
@@ -79,7 +84,7 @@ public class BulkPaymentSuccessfulSteps {
             });
 
         context.setActualResponse(response);
-    }*/
+    }
 
     @Then("^a successful response code and the appropriate bulk payment response data$")
     public void checkResponseCodeBulkPayment() throws IllegalArgumentException {
@@ -88,9 +93,7 @@ public class BulkPaymentSuccessfulSteps {
 
         assertThat(actualResponseList.getStatusCode(), equalTo(context.getTestData().getResponse().getHttpStatus()));
 
-        if (givenResponseBody.size() != actualResponseList.getBody().size()) {
-            throw new IllegalArgumentException("List of given response (" + givenResponseBody.size() + ") has a different size than list of actual response(" + actualResponseList.getBody().size() + ")");
-        }
+        assertThat(givenResponseBody.size(), equalTo(actualResponseList.getBody().size()));
 
         for (int i = 0; i < givenResponseBody.size(); ++i) {
             assertThat(actualResponseList.getBody().get(i).getTransactionStatus(), equalTo(givenResponseBody.get(i).getTransactionStatus()));
