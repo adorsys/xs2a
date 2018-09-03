a84d0368f74907910cf825ab115cdc07594afea3
Simon Schmoll <ssc@adorsys.de>
2018-09-03 17:19:03 +0200

    wrong input order for Given Statement, refactored exchange of entity, so that the headers are included

--- integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/pis/BulkPaymentSuccessfulSteps.java
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/pis/BulkPaymentSuccessfulSteps.java
@@ -57,7 +57,7 @@ public class BulkPaymentSuccessfulSteps {
     private ObjectMapper mapper;
 
     @Given("^PSU wants to initiate multiple payments (.*) using the payment service (.*) and the payment product (.*)$")
-    public void loadTestDataBulkPayment(String dataFileName, String paymentProduct, String paymentService) throws IOException {
+    public void loadTestDataBulkPayment(String dataFileName, String paymentService, String paymentProduct) throws IOException {
         context.setPaymentProduct(paymentProduct);
         context.setPaymentService(paymentService);
 
@@ -75,7 +75,7 @@ public class BulkPaymentSuccessfulSteps {
 
         ResponseEntity<List<PaymentInitationRequestResponse201>> response = restTemplate.exchange(
             context.getBaseUrl() + "/" + context.getPaymentService() + "/" + context.getPaymentProduct(),
-            HttpMethod.POST, new HttpEntity<>(entity), new ParameterizedTypeReference<List<PaymentInitationRequestResponse201>>() {
+            HttpMethod.POST, entity, new ParameterizedTypeReference<List<PaymentInitationRequestResponse201>>() {
             });
 
         context.setActualResponse(response);
