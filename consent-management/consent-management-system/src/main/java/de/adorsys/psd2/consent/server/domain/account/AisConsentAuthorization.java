0dd936771e2f69a2b646abf81d5dbc064dd2913b
Denys Golubiev <dgo@adorsys.de>
2018-10-05 16:03:02 +0200

    Migrate consent-management-system to de.adorsys.psd2 package structure

---
+++ consent-management/consent-management-system/src/main/java/de/adorsys/psd2/consent/server/domain/account/AisConsentAuthorization.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.psd2.consent.server.domain.account;
+
+import de.adorsys.psd2.consent.api.CmsScaStatus;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity(name = "ais_consent_authorization")
+public class AisConsentAuthorization {
+    @Id
+    @Column(name = "authorization_id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ais_consent_authorization_generator")
+    @SequenceGenerator(name = "ais_consent_authorization_generator", sequenceName = "ais_consent_authorization_id_seq")
+    private Long id;
+
+    @Column(name = "external_id", nullable = false)
+    private String externalId;
+
+    @Column(name = "psu_id")
+    private String psuId;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "consent_id")
+    private AisConsent consent;
+
+    @Column(name = "sca_status", nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private CmsScaStatus scaStatus;
+
+    @Column(name = "authentication_method_id")
+    private String authenticationMethodId;
+
+    @Column(name = "sca_authentication_data")
+    private String scaAuthenticationData;
+
+    @Column(name = "password")
+    private String password;
+}
