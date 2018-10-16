16c44d838c582beb6c0b2812cbaa66c57cae6d03
Simon Schmoll <ssc@adorsys.de>
2018-10-16 15:59:46 +0200

    refactored Global Errorful Steps

--- integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/pis/GlobalErrorfulSteps.java
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/pis/GlobalErrorfulSteps.java
@@ -16,22 +16,16 @@
 
 package de.adorsys.aspsp.xs2a.integtest.stepdefinitions.pis;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import de.adorsys.aspsp.xs2a.integtest.stepdefinitions.TestService;
 import de.adorsys.aspsp.xs2a.integtest.util.Context;
-import de.adorsys.aspsp.xs2a.integtest.util.PaymentUtils;
 import de.adorsys.psd2.model.TppMessages;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.client.RestClientResponseException;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.HashMap;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -45,25 +39,11 @@ public class GlobalErrorfulSteps {
     private Context context;
 
     @Autowired
-    private ObjectMapper mapper;
-
-    @Autowired
-    @Qualifier("xs2a")
-    private RestTemplate restTemplate;
+    private TestService testService;
 
     @When("^PSU sends the errorful update authorisation data request$")
     public void sendUpdateAuthorisationWithIdentificationRequest() throws IOException {
-        HttpEntity entity = PaymentUtils.getHttpEntity(
-            context.getTestData().getRequest(), context.getAccessToken());
-        try {
-            restTemplate.exchange(
-                context.getBaseUrl() + "/" + context.getPaymentService() + "/" + context.getPaymentId() + "/authorisations/" + context.getAuthorisationId(),
-                HttpMethod.PUT,
-                entity,
-                HashMap.class);
-        } catch (RestClientResponseException restClientException) {
-            context.handleRequestError(restClientException);
-        }
+        testService.sendErrorfulRestCall(HttpMethod.PUT, context.getBaseUrl() + "/" + context.getPaymentService() + "/" + context.getPaymentId() + "/authorisations/" + context.getAuthorisationId());
     }
 
     @Then("^an error response code and the appropriate error response are received")
