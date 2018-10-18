14c80a956ca7d55f80c74bbaf71165079af0928b
Petro Rudenko <pru@adorsys.com.ua>
2018-10-18 12:24:39 +0300

    change payment spi api

--- spi-api/src/main/java/de/adorsys/psd2/xs2a/spi/service/AisConsentSpi.java
+++ spi-api/src/main/java/de/adorsys/psd2/xs2a/spi/service/AisConsentSpi.java
@@ -17,6 +17,7 @@
 package de.adorsys.psd2.xs2a.spi.service;
 
 import de.adorsys.psd2.xs2a.spi.domain.account.SpiAccountConsent;
+import de.adorsys.psd2.xs2a.spi.domain.authorisation.SpiScaConfirmation;
 import de.adorsys.psd2.xs2a.spi.domain.consent.AspspConsentData;
 import de.adorsys.psd2.xs2a.spi.domain.psu.SpiPsuData;
 import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponse;
@@ -38,4 +39,16 @@ public interface AisConsentSpi extends AuthorisationSpi<SpiAccountConsent> {
         return SpiResponse.<VoidResponse>builder()
                    .fail(SpiResponseStatus.NOT_SUPPORTED);
     }
+
+    /**
+     * Sends authorisation confirmation information (secure code or such) to ASPSP and if case of successful validation executes payment at ASPSP. Used only with embedded SCA Approach.
+     *
+     * @param psuData            ASPSP identifier(s) of the psu
+     * @param spiScaConfirmation payment confirmation information
+     * @param aspspConsentData   Encrypted data that may stored in the consent management system in the consent linked to a request.
+     *                           May be null if consent does not contain such data, or request isn't done from a workflow with a consent
+     * @return Return a positive or negative response as part of SpiResponse
+     */
+    @NotNull
+    SpiResponse<VoidResponse> verifyAuthorisationCodeAndExecuteRequest(@NotNull SpiPsuData psuData, @NotNull SpiScaConfirmation spiScaConfirmation, @NotNull AspspConsentData aspspConsentData);
 }
--- spi-api/src/main/java/de/adorsys/psd2/xs2a/spi/service/PaymentAuthorisationSpi.java
+++ spi-api/src/main/java/de/adorsys/psd2/xs2a/spi/service/PaymentAuthorisationSpi.java
@@ -16,6 +16,7 @@
 
 package de.adorsys.psd2.xs2a.spi.service;
 
+import de.adorsys.psd2.xs2a.spi.domain.authorisation.SpiScaConfirmation;
 import de.adorsys.psd2.xs2a.spi.domain.consent.AspspConsentData;
 import de.adorsys.psd2.xs2a.spi.domain.psu.SpiPsuData;
 import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponse;
@@ -25,4 +26,17 @@ public interface PaymentAuthorisationSpi<T extends SpiPayment> extends Authorisa
 
     @NotNull
     SpiResponse<SpiResponse.VoidResponse> executePaymentWithoutSca(@NotNull SpiPsuData psuData, @NotNull T payment, @NotNull AspspConsentData aspspConsentData);
+
+    /**
+     * Sends authorisation confirmation information (secure code or such) to ASPSP and if case of successful validation executes payment at ASPSP. Used only with embedded SCA Approach.
+     *
+     * @param psuData            ASPSP identifier(s) of the psu
+     * @param spiScaConfirmation payment confirmation information
+     * @param payment     generic payment object
+     * @param aspspConsentData   Encrypted data that may stored in the consent management system in the consent linked to a request.
+     *                           May be null if consent does not contain such data, or request isn't done from a workflow with a consent
+     * @return Return a positive or negative response as part of SpiResponse
+     */
+    @NotNull
+    SpiResponse<SpiResponse.VoidResponse> verifyAuthorisationCodeAndExecuteRequest(@NotNull SpiPsuData psuData, @NotNull SpiScaConfirmation spiScaConfirmation, @NotNull T payment, @NotNull AspspConsentData aspspConsentData);
 }
