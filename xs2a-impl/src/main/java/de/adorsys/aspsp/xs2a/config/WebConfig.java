9cd85319b58782713b2282f6e07e9dcf06db3a69
Petro Rudenko <pru@adorsys.com.ua>
2018-10-26 14:01:36 +0300

    moved target proxy property to xs2a level

--- xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/config/WebConfig.java
+++ xs2a-impl/src/main/java/de/adorsys/aspsp/xs2a/config/WebConfig.java
@@ -37,6 +37,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.format.FormatterRegistry;
@@ -53,6 +54,7 @@ import java.time.LocalDateTime;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Value("${application.ais.transaction.max-length}")
     private int maxNumberOfCharInTransactionJson;
