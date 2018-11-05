d0914a662eb174b6e1a63ce7b26cc80a8a653b87
Denys Golubiev <dgo@adorsys.de>
2018-11-05 16:25:46 +0100

    Run spi-mock by default in remote mode. Move DB configuration to right place

--- consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/config/SchedulerConfig.java
+++ consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/config/SchedulerConfig.java
@@ -18,11 +18,13 @@ package de.adorsys.psd2.consent.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 @Configuration
+@EnableScheduling
 public class SchedulerConfig implements SchedulingConfigurer {
     @Value("${scheduler.pool.size:20}")
     private int poolSize;
