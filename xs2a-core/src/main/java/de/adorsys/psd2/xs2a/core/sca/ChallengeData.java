5b40f8a1b85c7149dfd5b58b0536b8f06860dab3
Alexander Geist <age@adorsys.de>
2018-11-19 10:19:36 +0100

    replaced lombok Value annotation with Data, AllArgsConstructor, NoArgsConstructor

--- xs2a-core/src/main/java/de/adorsys/psd2/xs2a/core/sca/ChallengeData.java
+++ xs2a-core/src/main/java/de/adorsys/psd2/xs2a/core/sca/ChallengeData.java
@@ -19,30 +19,34 @@ package de.adorsys.psd2.xs2a.core.sca;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.StringUtils;
 
-@Value
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @ApiModel(description = "Challenge data needed for SCA", value = "ChallengeData")
 public class ChallengeData {
     @ApiModelProperty(value = "PNG data (max. 512 kilobyte) to be displayed to the PSU, Base64 encoding, cp. [RFC4648]. This attribute is used only, when PHOTO_OTP or CHIP_OTP is the selected SCA method.")
-    private final byte[] image;
+    private byte[] image;
 
     @ApiModelProperty(value = "String challenge data", example = "zzz")
-    private final String data;
+    private String data;
 
     @ApiModelProperty(value = "A link where the ASPSP will provides the challenge image for the TPP", example = "https://www.testbank.com/authentication/image.jpg")
-    private final String imageLink;
+    private String imageLink;
 
     @ApiModelProperty(value = "The maximal length for the OTP to be typed in by the PSU", example = "6")
-    private final Integer otpMaxLength;
+    private Integer otpMaxLength;
 
     @ApiModelProperty(value = "The format type of the OTP to be typed in. The admitted values are 'characters' or 'integer'.", example = "integer")
-    private final OtpFormat otpFormat;
+    private OtpFormat otpFormat;
 
     @ApiModelProperty(value = "Additional explanation for the PSU to explain e.g. fallback mechanism for the chosen SCA method. The TPP is obliged to show this to the PSU.", example = "Additional information")
-    private final String additionalInformation;
+    private String additionalInformation;
 
     @JsonIgnore
     public boolean isEmpty() {
