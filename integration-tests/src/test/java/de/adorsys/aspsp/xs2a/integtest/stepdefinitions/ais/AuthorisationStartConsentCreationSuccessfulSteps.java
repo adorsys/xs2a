0fd706f0749349293a33f00f0ea20ab6c9258258
Mark Kram <mkr@adorsys.de>
2018-11-21 17:39:52 +0100

    create featurefile, json, stepdefinitions

---
+++ integration-tests/src/test/java/de/adorsys/aspsp/xs2a/integtest/stepdefinitions/ais/AuthorisationStartConsentCreationSuccessfulSteps.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018-2018 adorsys GmbH & Co KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.adorsys.aspsp.xs2a.integtest.stepdefinitions.ais;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+import de.adorsys.aspsp.xs2a.integtest.model.TestData;
+import de.adorsys.aspsp.xs2a.integtest.stepdefinitions.TestService;
+import de.adorsys.aspsp.xs2a.integtest.stepdefinitions.pis.FeatureFileSteps;
+import de.adorsys.aspsp.xs2a.integtest.util.Context;
+import de.adorsys.psd2.model.AccountDetails;
+import de.adorsys.psd2.model.StartScaprocessResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+@Slf4j
+@FeatureFileSteps
+public class AuthorisationStartConsentCreationSuccessfulSteps {
+
+    @Autowired
+    private Context context;
+
+    @Autowired
+    private TestService testService;
+
+    @And("^PSU wants to start the authorisation for consent creation using the authorisation data (.*)$")
+    public void loadAuthorisationData(String authorisationData) throws IOException {
+        testService.parseJson("/data-input/ais/embedded/" + authorisationData, new TypeReference<TestData<HashMap, StartScaprocessResponse>>() {
+        });
+    }
+
+    @When("^PSU sends the start authorisation request for consent creation$")
+    public void sendStartAuthorisationRequestForConsent(){
+        testService.sendRestCall(HttpMethod.POST,context.getBaseUrl()+"/consents/"+context.getConsentId()
+        +"/authorisations");
+    }
+
+}
