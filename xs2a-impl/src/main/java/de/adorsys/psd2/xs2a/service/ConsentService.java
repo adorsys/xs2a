db5e671550291c725894f3160c352f5be13f5427
ihorkuchevskiy <iku@adorsys.com.ua>
2018-11-28 19:29:42 +0200

    changed response code

--- xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/ConsentService.java
+++ xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/ConsentService.java
@@ -58,6 +58,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static de.adorsys.psd2.xs2a.core.consent.ConsentStatus.RECEIVED;
+import static de.adorsys.psd2.xs2a.domain.MessageErrorCode.RESOURCE_UNKNOWN_404;
 import static de.adorsys.psd2.xs2a.domain.consent.Xs2aAccountAccessType.ALL_ACCOUNTS;
 import static de.adorsys.psd2.xs2a.domain.consent.Xs2aAccountAccessType.ALL_ACCOUNTS_WITH_BALANCES;
 
@@ -328,7 +329,7 @@ public class ConsentService { //TODO change format of consentRequest to mandator
         return pisAuthorizationService.getCancellationAuthorisationSubResources(paymentId)
                    .map(resp -> ResponseObject.<Xs2aPaymentCancellationAuthorisationSubResource>builder().body(resp).build())
                    .orElseGet(ResponseObject.<Xs2aPaymentCancellationAuthorisationSubResource>builder()
-                                  .fail(new MessageError(MessageErrorCode.RESOURCE_UNKNOWN_404))
+                                  .fail(new MessageError(RESOURCE_UNKNOWN_404))
                                   ::build);
     }
 
@@ -338,7 +339,7 @@ public class ConsentService { //TODO change format of consentRequest to mandator
         return pisAuthorizationService.getAuthorisationSubResources(paymentId)
                    .map(resp -> ResponseObject.<Xs2aAuthorisationSubResource>builder().body(resp).build())
                    .orElseGet(ResponseObject.<Xs2aAuthorisationSubResource>builder()
-                                  .fail(new MessageError(MessageErrorCode.RESOURCE_UNKNOWN_404))
+                                  .fail(new MessageError(RESOURCE_UNKNOWN_404))
                                   ::build);
     }
 
@@ -348,7 +349,7 @@ public class ConsentService { //TODO change format of consentRequest to mandator
         return aisAuthorizationService.getAuthorisationSubResources(consentId)
                    .map(resp -> ResponseObject.<Xs2aAuthorisationSubResource>builder().body(resp).build())
                    .orElseGet(ResponseObject.<Xs2aAuthorisationSubResource>builder()
-                                  .fail(new MessageError(MessageErrorCode.CONSENT_UNKNOWN_400))
+                                  .fail(new MessageError(RESOURCE_UNKNOWN_404))
                                   ::build);
     }
 
