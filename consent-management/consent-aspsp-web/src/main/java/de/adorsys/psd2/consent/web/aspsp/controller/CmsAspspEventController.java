c0839f6eec93ed2a3fc72a392e2059a35aacf8fc
Petro Rudenko <pru@adorsys.com.ua>
2018-12-14 21:18:34 +0200

    changed requestHeader to required= false

--- consent-management/consent-aspsp-web/src/main/java/de/adorsys/psd2/consent/web/aspsp/controller/CmsAspspEventController.java
+++ consent-management/consent-aspsp-web/src/main/java/de/adorsys/psd2/consent/web/aspsp/controller/CmsAspspEventController.java
@@ -50,7 +50,7 @@ public class CmsAspspEventController {
         @RequestHeader(value = "end-date")
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) OffsetDateTime end,
         @ApiParam(value = "Bank instance ID")
-        @RequestHeader(value = "instance-id") String instanceId) {
+        @RequestHeader(value = "instance-id", required = false) String instanceId) {
         List<Event> events = cmsAspspEventService.getEventsForPeriod(start, end, instanceId);
         return new ResponseEntity<>(events, HttpStatus.OK);
     }
