47d50ce15917cf87034a343149bd865bc4bfc1d6
may <mpo@adorsys.com.ua>
2019-02-01 15:15:10 +0200

    fixed pmd check

--- consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/psu/CmsPsuPisServiceInternal.java
+++ consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/psu/CmsPsuPisServiceInternal.java
@@ -209,16 +209,6 @@ public class CmsPsuPisServiceInternal implements CmsPsuPisService {
         return true;
     }
 
-    private boolean isAuthorisationValidForPsuAndStatus(PsuIdData givenPsuIdData, PisAuthorization authorization) {
-        if (authorization.getScaStatus().isFinalisedStatus()) {
-            return false;
-        }
-        PsuIdData actualPsuIdData = psuDataMapper.mapToPsuIdData(authorization.getPsuData());
-        return Optional.ofNullable(actualPsuIdData)
-                   .map(givenPsuIdData::contentEquals)
-                   .orElse(false);
-    }
-
     private CmsPaymentResponse buildCmsPaymentResponse(PisAuthorization authorisation) {
         PisCommonPaymentData commonPayment = authorisation.getPaymentData();
         CmsPayment payment = cmsPsuPisMapper.mapToCmsPayment(commonPayment.getPayments());
