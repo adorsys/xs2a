14ec5f7c8d406fa082f4fa1cbf2e7275c553717a
Denys Golubiev <dgo@adorsys.de>
2019-02-06 17:44:21 +0200

    Fix NPE
    #593

--- spi-api/src/main/java/de/adorsys/psd2/xs2a/spi/domain/authorisation/SpiAuthorizationCodeResult.java
+++ spi-api/src/main/java/de/adorsys/psd2/xs2a/spi/domain/authorisation/SpiAuthorizationCodeResult.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2018 adorsys GmbH & Co KG
+ * Copyright 2018-2019 adorsys GmbH & Co KG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,7 +25,7 @@ public class SpiAuthorizationCodeResult {
     private SpiAuthenticationObject selectedScaMethod;
 
     public boolean isEmpty() {
-        return challengeData.isEmpty()
+        return (challengeData == null || challengeData.isEmpty())
             && selectedScaMethod == null;
     }
 }
