2ba829313a55a084d92596c18b368d43f2194ff7
Kerus <kerusm1497@gmail.com>
2019-02-15 17:38:08 +0200

    Add two test / 100% coverage

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/payment/ReadPeriodicPaymentServiceTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/payment/ReadPeriodicPaymentServiceTest.java
@@ -4,15 +4,20 @@ import de.adorsys.psd2.consent.api.pis.PisPayment;
 import de.adorsys.psd2.xs2a.core.consent.AspspConsentData;
 import de.adorsys.psd2.xs2a.core.psu.PsuIdData;
 import de.adorsys.psd2.xs2a.core.tpp.TppInfo;
+import de.adorsys.psd2.xs2a.domain.ErrorHolder;
+import de.adorsys.psd2.xs2a.domain.MessageErrorCode;
 import de.adorsys.psd2.xs2a.domain.pis.PaymentInformationResponse;
 import de.adorsys.psd2.xs2a.domain.pis.PeriodicPayment;
 import de.adorsys.psd2.xs2a.service.consent.PisAspspDataService;
 import de.adorsys.psd2.xs2a.service.context.SpiContextDataProvider;
+import de.adorsys.psd2.xs2a.service.mapper.psd2.ServiceType;
+import de.adorsys.psd2.xs2a.service.mapper.spi_xs2a_mappers.SpiErrorMapper;
 import de.adorsys.psd2.xs2a.service.mapper.spi_xs2a_mappers.SpiToXs2aPeriodicPaymentMapper;
 import de.adorsys.psd2.xs2a.spi.domain.SpiContextData;
 import de.adorsys.psd2.xs2a.spi.domain.payment.SpiPeriodicPayment;
 import de.adorsys.psd2.xs2a.spi.domain.psu.SpiPsuData;
 import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponse;
+import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponseStatus;
 import de.adorsys.psd2.xs2a.spi.service.PeriodicPaymentSpi;
 import org.junit.Before;
 import org.junit.Test;
@@ -22,6 +27,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,6 +61,8 @@ public class ReadPeriodicPaymentServiceTest {
     private SpiToXs2aPeriodicPaymentMapper spiToXs2aPeriodicPaymentMapper;
     @Mock
     private SpiPaymentFactory spiPaymentFactory;
+    @Mock
+    private SpiErrorMapper spiErrorMapper;
 
     @Before
     public void init() {
@@ -66,7 +74,6 @@ public class ReadPeriodicPaymentServiceTest {
                 .payload(SPI_PERIODIC_PAYMENT)
                 .success());
         when(spiToXs2aPeriodicPaymentMapper.mapToXs2aPeriodicPayment(SPI_PERIODIC_PAYMENT)).thenReturn(PERIODIC_PAYMENT);
-
     }
 
     @Test
@@ -90,6 +97,9 @@ public class ReadPeriodicPaymentServiceTest {
         //given
         when(updatePaymentStatusAfterSpiService.updatePaymentStatus(SOME_ASPSP_CONSENT_DATA.getConsentId(), PERIODIC_PAYMENT.getTransactionStatus()))
             .thenReturn(false);
+        ErrorHolder expectedError = ErrorHolder.builder(MessageErrorCode.FORMAT_ERROR)
+            .messages(Collections.singletonList("Payment is finalised already, so its status cannot be changed"))
+            .build();
 
         //when
         PaymentInformationResponse<PeriodicPayment> actualResponse = readPeriodicPaymentService.getPayment(PIS_PAYMENTS, PRODUCT, PSU_DATA, SOME_ASPSP_CONSENT_DATA);
@@ -98,6 +108,48 @@ public class ReadPeriodicPaymentServiceTest {
         assertThat(actualResponse.hasError()).isTrue();
         assertThat(actualResponse.getPayment()).isNull();
         assertThat(actualResponse.getErrorHolder()).isNotNull();
+        assertThat(actualResponse.getErrorHolder()).isEqualToComparingFieldByField(expectedError);
+    }
+
+    @Test
+    public void getPayment_spiPaymentFactory_createSpiPeriodicPayment_Failed() {
+        //given
+        when(spiPaymentFactory.createSpiPeriodicPayment(PIS_PAYMENTS.get(0), PRODUCT)).thenReturn(Optional.empty());
+        ErrorHolder expectedError = ErrorHolder.builder(MessageErrorCode.RESOURCE_UNKNOWN_404)
+            .messages(Collections.singletonList("Payment not found"))
+            .build();
+
+        //when
+        PaymentInformationResponse<PeriodicPayment> actualResponse = readPeriodicPaymentService.getPayment(PIS_PAYMENTS, PRODUCT, PSU_DATA, SOME_ASPSP_CONSENT_DATA);
+
+        //then
+        assertThat(actualResponse.hasError()).isTrue();
+        assertThat(actualResponse.getPayment()).isNull();
+        assertThat(actualResponse.getErrorHolder()).isNotNull();
+        assertThat(actualResponse.getErrorHolder()).isEqualToComparingFieldByField(expectedError);
+    }
+
+    @Test
+    public void getPayment_periodicPaymentSpi_getPaymentById_Failed() {
+        //given
+        SpiResponse<SpiPeriodicPayment> spiResponseError = SpiResponse.<SpiPeriodicPayment>builder()
+            .aspspConsentData(SOME_ASPSP_CONSENT_DATA)
+            .fail(SpiResponseStatus.LOGICAL_FAILURE);
+        ErrorHolder expectedError = ErrorHolder.builder(MessageErrorCode.RESOURCE_UNKNOWN_404)
+            .messages(Collections.singletonList("Payment not found"))
+            .build();
+        when(periodicPaymentSpi.getPaymentById(SPI_CONTEXT_DATA, SPI_PERIODIC_PAYMENT, SOME_ASPSP_CONSENT_DATA))
+            .thenReturn(spiResponseError);
+        when(spiErrorMapper.mapToErrorHolder(spiResponseError, ServiceType.PIS)).thenReturn(expectedError);
+
+        //when
+        PaymentInformationResponse<PeriodicPayment> actualResponse = readPeriodicPaymentService.getPayment(PIS_PAYMENTS, PRODUCT, PSU_DATA, SOME_ASPSP_CONSENT_DATA);
+
+        //then
+        assertThat(actualResponse.hasError()).isTrue();
+        assertThat(actualResponse.getPayment()).isNull();
+        assertThat(actualResponse.getErrorHolder()).isNotNull();
+        assertThat(actualResponse.getErrorHolder()).isEqualToComparingFieldByField(expectedError);
     }
 
     private SpiContextData getSpiContextData() {
