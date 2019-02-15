418ec8485ce4e5007054393608be592f87f0c4a4
Kerus <kerusm1497@gmail.com>
2019-02-15 10:37:25 +0200

    Add failure test / cleaning code

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/payment/ReadPeriodicPaymentServiceTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/payment/ReadPeriodicPaymentServiceTest.java
@@ -1,34 +1,29 @@
 package de.adorsys.psd2.xs2a.service.payment;
 
 import de.adorsys.psd2.consent.api.pis.PisPayment;
-import de.adorsys.psd2.consent.api.pis.proto.PisPaymentInfo;
 import de.adorsys.psd2.xs2a.core.consent.AspspConsentData;
-import de.adorsys.psd2.xs2a.core.pis.TransactionStatus;
 import de.adorsys.psd2.xs2a.core.psu.PsuIdData;
 import de.adorsys.psd2.xs2a.core.tpp.TppInfo;
-import de.adorsys.psd2.xs2a.domain.ErrorHolder;
-import de.adorsys.psd2.xs2a.domain.MessageErrorCode;
 import de.adorsys.psd2.xs2a.domain.pis.PaymentInformationResponse;
 import de.adorsys.psd2.xs2a.domain.pis.PeriodicPayment;
 import de.adorsys.psd2.xs2a.service.consent.PisAspspDataService;
 import de.adorsys.psd2.xs2a.service.context.SpiContextDataProvider;
-import de.adorsys.psd2.xs2a.service.mapper.spi_xs2a_mappers.SpiErrorMapper;
 import de.adorsys.psd2.xs2a.service.mapper.spi_xs2a_mappers.SpiToXs2aPeriodicPaymentMapper;
 import de.adorsys.psd2.xs2a.spi.domain.SpiContextData;
 import de.adorsys.psd2.xs2a.spi.domain.payment.SpiPeriodicPayment;
 import de.adorsys.psd2.xs2a.spi.domain.psu.SpiPsuData;
 import de.adorsys.psd2.xs2a.spi.domain.response.SpiResponse;
 import de.adorsys.psd2.xs2a.spi.service.PeriodicPaymentSpi;
-import org.jetbrains.annotations.NotNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.ArgumentMatcher;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
@@ -38,13 +33,10 @@ public class ReadPeriodicPaymentServiceTest {
 
     private static final String PRODUCT = "sepa-credit-transfers";
     private static final PsuIdData PSU_DATA = new PsuIdData(null, null, null, null);
-    private final List<PisPayment> PIS_PAYMETNS = getListPisPayment();
-    private final PisPayment PIS_PAYMENT = getPisPayment();
+    private final List<PisPayment> PIS_PAYMENTS = getListPisPayment();
     private final SpiContextData SPI_CONTEXT_DATA = getSpiContextData();
     private final SpiPeriodicPayment SPI_PERIODIC_PAYMENT = new SpiPeriodicPayment(PRODUCT);
     private final PeriodicPayment PERIODIC_PAYMENT = new PeriodicPayment();
-    private final PaymentInformationResponse PAYMENT_INFORMATION_REPSONSE = getPaymentInformationResponse();
-    private final PisPaymentInfo PIS_PAYMENT_INFO = getPisPaymentInfo();
 
     private static final AspspConsentData SOME_ASPSP_CONSENT_DATA = new AspspConsentData(new byte[0], "some consent id");
 
@@ -62,13 +54,11 @@ public class ReadPeriodicPaymentServiceTest {
     @Mock
     private SpiToXs2aPeriodicPaymentMapper spiToXs2aPeriodicPaymentMapper;
     @Mock
-    private SpiErrorMapper spiErrorMapper;
-    @Mock
     private SpiPaymentFactory spiPaymentFactory;
 
     @Before
     public void init() {
-        when(spiPaymentFactory.createSpiPeriodicPayment(PIS_PAYMETNS.get(0), PRODUCT)).thenReturn(Optional.of(SPI_PERIODIC_PAYMENT));
+        when(spiPaymentFactory.createSpiPeriodicPayment(PIS_PAYMENTS.get(0), PRODUCT)).thenReturn(Optional.of(SPI_PERIODIC_PAYMENT));
         when(spiContextDataProvider.provideWithPsuIdData(PSU_DATA)).thenReturn(SPI_CONTEXT_DATA);
         when(periodicPaymentSpi.getPaymentById(SPI_CONTEXT_DATA, SPI_PERIODIC_PAYMENT, SOME_ASPSP_CONSENT_DATA))
             .thenReturn(SpiResponse.<SpiPeriodicPayment>builder()
@@ -76,17 +66,30 @@ public class ReadPeriodicPaymentServiceTest {
                 .payload(SPI_PERIODIC_PAYMENT)
                 .success());
         when(spiToXs2aPeriodicPaymentMapper.mapToXs2aPeriodicPayment(SPI_PERIODIC_PAYMENT)).thenReturn(PERIODIC_PAYMENT);
-        when(updatePaymentStatusAfterSpiService.updatePaymentStatus(SOME_ASPSP_CONSENT_DATA.getConsentId(), PERIODIC_PAYMENT.getTransactionStatus()))
-            .thenReturn(true);
+
     }
 
     @Test
-    public void getPayment() {
+    public void getPayment_Success() {
         //when
-        PaymentInformationResponse<PeriodicPayment> actualRespone = readPeriodicPaymentService.getPayment(PIS_PAYMETNS, PRODUCT, PSU_DATA, SOME_ASPSP_CONSENT_DATA);
+        when(updatePaymentStatusAfterSpiService.updatePaymentStatus(SOME_ASPSP_CONSENT_DATA.getConsentId(), PERIODIC_PAYMENT.getTransactionStatus()))
+            .thenReturn(true);
+        PaymentInformationResponse<PeriodicPayment> actualResponse = readPeriodicPaymentService.getPayment(PIS_PAYMENTS, PRODUCT, PSU_DATA, SOME_ASPSP_CONSENT_DATA);
 
         //then
-        assertThat(actualRespone.hasError()).isFalse();
+        assertThat(actualResponse.hasError()).isFalse();
+    }
+
+    @Test
+    public void getPayment_Failed() {
+        //when
+        when(updatePaymentStatusAfterSpiService.updatePaymentStatus(SOME_ASPSP_CONSENT_DATA.getConsentId(), PERIODIC_PAYMENT.getTransactionStatus()))
+            .thenReturn(false);
+        PaymentInformationResponse<PeriodicPayment> actualResponse = readPeriodicPaymentService.getPayment(PIS_PAYMENTS, PRODUCT, PSU_DATA, SOME_ASPSP_CONSENT_DATA);
+
+        //then
+        assertThat(actualResponse.hasError()).isTrue();
+        assertThat(actualResponse.getErrorHolder()).isNotNull();
     }
 
     private SpiContextData getSpiContextData() {
@@ -96,26 +99,10 @@ public class ReadPeriodicPaymentServiceTest {
         );
     }
 
-    private PisPayment getPisPayment() {
-        return new PisPayment();
-    }
-
     private List<PisPayment> getListPisPayment() {
         List<PisPayment> list = new ArrayList<PisPayment>();
         PisPayment pisPayment = new PisPayment();
         list.add(pisPayment);
         return list;
-    }
-
-    private PaymentInformationResponse getPaymentInformationResponse() {
-        return new PaymentInformationResponse<>(
-            ErrorHolder.builder(MessageErrorCode.FORMAT_ERROR)
-                .messages(Collections.singletonList("Payment is finalised already, so its status cannot be changed"))
-                .build()
-        );
-    }
-
-    private PisPaymentInfo getPisPaymentInfo() {
-        return new PisPaymentInfo();
     }
 }
