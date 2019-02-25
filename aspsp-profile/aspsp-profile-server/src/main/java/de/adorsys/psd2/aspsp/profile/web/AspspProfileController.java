ee3d4b155bfe85b1a9c64bd7f2381540ad3a3333
Denys Golubiev <dgo@adorsys.de>
2019-02-25 11:22:51 +0100

    Fix links to documentation and adorsys page

--- aspsp-profile/aspsp-profile-server/src/main/java/de/adorsys/psd2/aspsp/profile/web/AspspProfileController.java
+++ aspsp-profile/aspsp-profile-server/src/main/java/de/adorsys/psd2/aspsp/profile/web/AspspProfileController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2018 adorsys GmbH & Co KG
+ * Copyright 2018-2019 adorsys GmbH & Co KG
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -33,7 +33,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping(path = "aspsp-profile")
+@RequestMapping(path = "/api/v1/aspsp-profile")
 @Api(value = "Aspsp profile", tags = "Aspsp profile", description = "Provides access to aspsp profile")
 public class AspspProfileController {
 
