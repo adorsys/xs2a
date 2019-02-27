3125d03921027370c9186975c209c1f9b54839a2
Kerus <kerusm1497@gmail.com>
2019-02-27 22:01:11 +0200

    Add DecoupledAisAuthorizationServiceTest

---
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ais/DecoupledAisAuthorizationServiceTest.java
@@ -0,0 +1,32 @@
+package de.adorsys.psd2.xs2a.service.authorization.ais;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DecoupledAisAuthorizationServiceTest {
+
+    @Test
+    public void createConsentAuthorization() {
+    }
+
+    @Test
+    public void updateConsentPsuData() {
+    }
+
+    @Test
+    public void getAccountConsentAuthorizationById() {
+    }
+
+    @Test
+    public void getAuthorisationSubResources() {
+    }
+
+    @Test
+    public void getAuthorisationScaStatus() {
+    }
+
+    @Test
+    public void getScaApproachServiceType() {
+    }
+}
