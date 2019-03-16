0ffdb5f29a70f26f256adf5f9dc50e475b757d12
KolomoetsAA <favcr5@gmail.com>
2019-03-16 12:20:09 +0200

    Refactoring code with new updates

---
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ScaServiceResolverTest.java
@@ -0,0 +1,27 @@
+package de.adorsys.psd2.xs2a.service.authorization;
+
+import de.adorsys.psd2.xs2a.core.profile.ScaApproach;
+import de.adorsys.psd2.xs2a.service.ScaApproachResolver;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ScaServiceResolverTest {
+//    @InjectMocks
+    private ScaServiceResolver scaServiceResolver;
+//
+//    @Mock
+//    private List<T> services;
+//    @Mock
+//    private ScaApproachResolver scaApproachResolver;
+//    @Mock
+//    private Map<ScaApproach, T> SERVICE_CONTAINER = new HashMap<>();
+}
--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ais/CommonDecoupledAisServiceTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ais/CommonDecoupledAisServiceTest.java
@@ -185,7 +185,6 @@ public class CommonDecoupledAisServiceTest {
 
     private static UpdateConsentPsuDataResponse buildUpdateConsentPsuDataResponse() {
         UpdateConsentPsuDataResponse response = new UpdateConsentPsuDataResponse();
-        response.setPsuId(PSU_ID);
         response.setPsuMessage(PSU_SUCCESS_MESSAGE);
         response.setScaStatus(METHOD_SELECTED_SCA_STATUS);
         response.setChosenScaMethod(new Xs2aAuthenticationObject());
