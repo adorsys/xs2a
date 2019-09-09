0203eb4162095fb984c841ecca3d0fef50dfe9c6
Kateryna Yashnyk <kya@adorsys.com.ua>
2019-09-09 10:29:55 +0000

    limited the scope of CmsAspspExceptionHandler to controllers in de.adorsys.psd2.consent.web.aspsp.controller

--- consent-management/consent-aspsp-web/src/test/java/de/adorsys/psd2/consent/web/aspsp/controller/CmsAspspPisExportControllerTest.java
+++ consent-management/consent-aspsp-web/src/test/java/de/adorsys/psd2/consent/web/aspsp/controller/CmsAspspPisExportControllerTest.java
@@ -16,160 +16,128 @@
 
 package de.adorsys.psd2.consent.web.aspsp.controller;
 
+import de.adorsys.psd2.consent.api.CmsAddress;
+import de.adorsys.psd2.consent.api.pis.CmsAmount;
 import de.adorsys.psd2.consent.api.pis.CmsPayment;
 import de.adorsys.psd2.consent.api.pis.CmsSinglePayment;
 import de.adorsys.psd2.consent.aspsp.api.pis.CmsAspspPisExportService;
 import de.adorsys.psd2.xs2a.core.psu.PsuIdData;
+import de.adorsys.xs2a.reader.JsonReader;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Currency;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CmsAspspPisExportControllerTest {
-    private static final String PAYMENT_ID = "payment id";
-    private static final String PAYMENT_PRODUCT = "sepa-credit-transfers";
-    private static final String TPP_AUTHORISATION_NUMBER = "authorisation number";
-    private static final String WRONG_TPP_AUTHORISATION_NUMBER = "wrong authorisation number";
-    private static final LocalDate CREATION_DATE_FROM = LocalDate.of(2019, 1, 1);
-    private static final LocalDate CREATION_DATE_TO = LocalDate.of(2020, 12, 1);
-    private static final String DEFAULT_SERVICE_INSTANCE_ID = "UNDEFINED";
-    private static final String PSU_ID = "psu id";
-    private static final String WRONG_PSU_ID = "wrong psu id";
-    private static final String ASPSP_ACCOUNT_ID = "account id";
-    private static final String WRONG_ASPSP_ACCOUNT_ID = "wrong account id";
+    private final String PSU_ID = "marion.mueller";
+    private final String TPP_ID = "PSDDE-FAKENCA-87B2AC";
+    private final String ACCOUNT_ID = "account_id";
+    private final String EXPORT_PIS_CONSENT_BY_TPP = "/aspsp-api/v1/pis/payments/tpp/PSDDE-FAKENCA-87B2AC";
+    private final String EXPORT_PIS_CONSENT_BY_PSU = "/aspsp-api/v1/pis/payments/psu";
+    private final String EXPORT_PIS_CONSENT_BY_ACCOUNT = "/aspsp-api/v1/pis/payments/account/account_id";
+    private final LocalDate START_DATE = LocalDate.of(2019, 2, 25);
+    private final LocalDate END_DATE = LocalDate.of(2020, 7, 22);
+    private final String INSTANCE_ID = "UNDEFINED";
+    private final String LIST_OF_PIIS_CONSENTS_PATH = "json/pis/list-pis-payment.json";
 
-    @Mock
-    private CmsAspspPisExportService cmsAspspPisExportService;
+    private MockMvc mockMvc;
+    private JsonReader jsonReader = new JsonReader();
+    private HttpHeaders httpHeaders = new HttpHeaders();
+    private PsuIdData psuIdData;
+    private CmsPayment cmsPayment;
+    private Collection<CmsPayment> cmsPayments;
+
     @InjectMocks
     private CmsAspspPisExportController cmsAspspPisExportController;
 
+    @Mock
+    private CmsAspspPisExportService cmsAspspPisExportService;
+
     @Before
     public void setUp() {
-        PsuIdData psuIdData = buildPsuIdData(PSU_ID);
-        when(cmsAspspPisExportService.exportPaymentsByTpp(eq(TPP_AUTHORISATION_NUMBER), eq(CREATION_DATE_FROM),
-                                                          eq(CREATION_DATE_TO), eq(psuIdData), eq(DEFAULT_SERVICE_INSTANCE_ID)))
-            .thenReturn(Collections.singletonList(buildCmsPayment()));
-        when(cmsAspspPisExportService.exportPaymentsByTpp(eq(WRONG_TPP_AUTHORISATION_NUMBER), any(), any(), any(), any()))
-            .thenReturn(Collections.emptyList());
+        psuIdData = jsonReader.getObjectFromFile("json/psu-id-data.json", PsuIdData.class);
+        cmsPayment = getCmsPayment();
+        cmsPayments = Collections.singletonList(cmsPayment);
 
-        when(cmsAspspPisExportService.exportPaymentsByPsu(eq(psuIdData), eq(CREATION_DATE_FROM), eq(CREATION_DATE_TO), eq(DEFAULT_SERVICE_INSTANCE_ID)))
-            .thenReturn(Collections.singletonList(buildCmsPayment()));
-        PsuIdData wrongPsuIdData = buildPsuIdData(WRONG_PSU_ID);
-        when(cmsAspspPisExportService.exportPaymentsByPsu(eq(wrongPsuIdData), any(), any(), any()))
-            .thenReturn(Collections.emptyList());
+        httpHeaders.add("psu-id", PSU_ID);
+        httpHeaders.add("Content-Type", "application/json");
+        httpHeaders.add("Start-Date", START_DATE.toString());
+        httpHeaders.add("End-Date", END_DATE.toString());
+        httpHeaders.add("instance-id", INSTANCE_ID);
 
-        when(cmsAspspPisExportService.exportPaymentsByAccountId(eq(ASPSP_ACCOUNT_ID), eq(CREATION_DATE_FROM), eq(CREATION_DATE_TO), eq(DEFAULT_SERVICE_INSTANCE_ID)))
-            .thenReturn(Collections.singletonList(buildCmsPayment()));
-        when(cmsAspspPisExportService.exportPaymentsByAccountId(eq(WRONG_ASPSP_ACCOUNT_ID), any(), any(), any()))
-            .thenReturn(Collections.emptyList());
-
+        mockMvc = MockMvcBuilders.standaloneSetup(cmsAspspPisExportController).build();
     }
 
     @Test
-    public void getPaymentsByTpp_success() {
-        // Given
-        Collection<CmsPayment> expected = Collections.singletonList(buildCmsPayment());
+    public void getPaymentsByTpp() throws Exception {
+        when(cmsAspspPisExportService.exportPaymentsByTpp(TPP_ID, START_DATE, END_DATE, psuIdData, INSTANCE_ID))
+            .thenReturn(cmsPayments);
 
-        // When
-        ResponseEntity<Collection<CmsPayment>> actual =
-            cmsAspspPisExportController.getPaymentsByTpp(TPP_AUTHORISATION_NUMBER, CREATION_DATE_FROM,
-                                                         CREATION_DATE_TO, PSU_ID, null,
-                                                         null, null, DEFAULT_SERVICE_INSTANCE_ID);
+        mockMvc.perform(get(EXPORT_PIS_CONSENT_BY_TPP)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                            .headers(httpHeaders))
+            .andExpect(status().is(HttpStatus.OK.value()))
+            .andExpect(content().json(jsonReader.getStringFromFile(LIST_OF_PIIS_CONSENTS_PATH)));
 
-        // Then
-        assertEquals(HttpStatus.OK, actual.getStatusCode());
-        assertEquals(expected, actual.getBody());
+        verify(cmsAspspPisExportService, times(1)).exportPaymentsByTpp(TPP_ID, START_DATE, END_DATE, psuIdData, INSTANCE_ID);
     }
 
     @Test
-    public void getPaymentsByTpp_failure_wrongTppAuthorisationNumber() {
-        // When
-        ResponseEntity<Collection<CmsPayment>> actual =
-            cmsAspspPisExportController.getPaymentsByTpp(WRONG_TPP_AUTHORISATION_NUMBER, CREATION_DATE_FROM,
-                                                         CREATION_DATE_TO, PSU_ID, null,
-                                                         null, null, DEFAULT_SERVICE_INSTANCE_ID);
+    public void getPaymentsByPsu() throws Exception {
+        when(cmsAspspPisExportService.exportPaymentsByPsu(psuIdData, START_DATE, END_DATE, INSTANCE_ID))
+            .thenReturn(cmsPayments);
 
-        // Then
-        assertEquals(HttpStatus.OK, actual.getStatusCode());
-        assertEquals(Collections.emptyList(), actual.getBody());
+        mockMvc.perform(get(EXPORT_PIS_CONSENT_BY_PSU)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                            .headers(httpHeaders))
+            .andExpect(status().is(HttpStatus.OK.value()))
+            .andExpect(content().json(jsonReader.getStringFromFile(LIST_OF_PIIS_CONSENTS_PATH)));
+
+        verify(cmsAspspPisExportService, times(1)).exportPaymentsByPsu(psuIdData, START_DATE, END_DATE, INSTANCE_ID);
     }
 
     @Test
-    public void getPaymentsByPsu_success() {
-        // Given
-        Collection<CmsPayment> expected = Collections.singletonList(buildCmsPayment());
+    public void getPaymentsByAccountId() throws Exception {
+        when(cmsAspspPisExportService.exportPaymentsByAccountId(ACCOUNT_ID, START_DATE, END_DATE, INSTANCE_ID))
+            .thenReturn(cmsPayments);
 
-        // When
-        ResponseEntity<Collection<CmsPayment>> actual =
-            cmsAspspPisExportController.getPaymentsByPsu(CREATION_DATE_FROM, CREATION_DATE_TO, PSU_ID, null,
-                                                         null, null, DEFAULT_SERVICE_INSTANCE_ID);
+        mockMvc.perform(get(EXPORT_PIS_CONSENT_BY_ACCOUNT)
+                            .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                            .headers(httpHeaders))
+            .andExpect(status().is(HttpStatus.OK.value()))
+            .andExpect(content().json(jsonReader.getStringFromFile(LIST_OF_PIIS_CONSENTS_PATH)));
 
-        // Then
-        assertEquals(HttpStatus.OK, actual.getStatusCode());
-        assertEquals(expected, actual.getBody());
+        verify(cmsAspspPisExportService, times(1)).exportPaymentsByAccountId(ACCOUNT_ID, START_DATE, END_DATE, INSTANCE_ID);
     }
 
-    @Test
-    public void getPaymentsByPsu_failure_wrongPsuIdData() {
-        // When
-        ResponseEntity<Collection<CmsPayment>> actual =
-            cmsAspspPisExportController.getPaymentsByPsu(CREATION_DATE_FROM, CREATION_DATE_TO, WRONG_PSU_ID, null,
-                                                         null, null, DEFAULT_SERVICE_INSTANCE_ID);
-
-        // Then
-        assertEquals(HttpStatus.OK, actual.getStatusCode());
-        assertEquals(Collections.emptyList(), actual.getBody());
-    }
-
-    @Test
-    public void getPaymentsByAccountId_success() {
-        // Given
-        Collection<CmsPayment> expected = Collections.singletonList(buildCmsPayment());
-
-        // When
-        ResponseEntity<Collection<CmsPayment>> actual =
-            cmsAspspPisExportController.getPaymentsByAccountId(ASPSP_ACCOUNT_ID, CREATION_DATE_FROM, CREATION_DATE_TO,
-                                                               DEFAULT_SERVICE_INSTANCE_ID);
-
-        // Then
-        assertEquals(HttpStatus.OK, actual.getStatusCode());
-        assertEquals(expected, actual.getBody());
-    }
-
-    @Test
-    public void getPaymentsByAccountId_failure_wrongAccountId() {
-        // When
-        ResponseEntity<Collection<CmsPayment>> actual =
-            cmsAspspPisExportController.getPaymentsByAccountId(WRONG_ASPSP_ACCOUNT_ID, CREATION_DATE_FROM, CREATION_DATE_TO,
-                                                               DEFAULT_SERVICE_INSTANCE_ID);
-
-        // Then
-        assertEquals(HttpStatus.OK, actual.getStatusCode());
-        assertEquals(Collections.emptyList(), actual.getBody());
-    }
-
-    private CmsPayment buildCmsPayment() {
-        CmsSinglePayment cmsPayment = new CmsSinglePayment(PAYMENT_PRODUCT);
-        cmsPayment.setPaymentId(PAYMENT_ID);
-
-        return cmsPayment;
-    }
-
-    private PsuIdData buildPsuIdData(String id) {
-        return new PsuIdData(id, null, null, null);
+    private CmsPayment getCmsPayment() {
+        String paymentProduct = "paymentProduct";
+        CmsSinglePayment result = new CmsSinglePayment(paymentProduct);
+        result.setEndToEndIdentification("RI-1234567890");
+        result.setInstructedAmount(new CmsAmount(Currency.getInstance("EUR"), BigDecimal.valueOf(1000)));
+        result.setCreditorAgent("agent");
+        result.setCreditorName("name");
+        result.setCreditorAddress(new CmsAddress());
+        result.setRemittanceInformationUnstructured("remittanceInformationUnstructured");
+        return result;
     }
 }
