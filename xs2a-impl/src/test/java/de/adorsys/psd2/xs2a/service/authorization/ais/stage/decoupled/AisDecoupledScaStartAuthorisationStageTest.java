4fb2f1602695f1bfeee4165f64a816e8f454d1e4
iiuex <iiu@adorsys.com.ua>
2019-09-24 10:34:40 +0300

    #1053 Added check the authorisation result of AuthorisationSpi#authorisePsu

--- xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ais/stage/decoupled/AisDecoupledScaStartAuthorisationStageTest.java
+++ xs2a-impl/src/test/java/de/adorsys/psd2/xs2a/service/authorization/ais/stage/decoupled/AisDecoupledScaStartAuthorisationStageTest.java
@@ -172,8 +172,12 @@ public class AisDecoupledScaStartAuthorisationStageTest {
 
     @Test
     public void apply_Failure_AuthorisationStatusSpiResponseFailed() {
+        SpiResponse<SpiAuthorisationStatus> spiResponse = SpiResponse.<SpiAuthorisationStatus>builder()
+                                                              .payload(SpiAuthorisationStatus.FAILURE)
+                                                              .build();
+
         when(aisConsentSpi.authorisePsu(SPI_CONTEXT_DATA, SPI_PSU_DATA, PASSWORD, spiAccountConsent, spiAspspConsentDataProvider))
-            .thenReturn(buildErrorSpiResponse(SpiAuthorisationStatus.FAILURE));
+            .thenReturn(spiResponse);
 
         UpdateConsentPsuDataResponse actualResponse = scaReceivedAuthorisationStage.apply(request);
 
