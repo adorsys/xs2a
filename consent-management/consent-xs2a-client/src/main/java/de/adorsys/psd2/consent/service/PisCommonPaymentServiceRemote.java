606891f3fc17d18668cb7be661e8fae583aae89c
Kateryna Yashnyk <kya@adorsys.com.ua>
2019-10-23 12:54:07 +0000

    #1066 Incorrect response for Update PSU data request with incorrect PSU-ID in header: added postman tests

--- consent-management/consent-xs2a-client/src/main/java/de/adorsys/psd2/consent/service/PisCommonPaymentServiceRemote.java
+++ consent-management/consent-xs2a-client/src/main/java/de/adorsys/psd2/consent/service/PisCommonPaymentServiceRemote.java
@@ -92,9 +92,14 @@ public class PisCommonPaymentServiceRemote implements PisCommonPaymentServiceEnc
 
     @Override
     public Optional<CreatePisAuthorisationResponse> createAuthorization(String paymentId, CreatePisAuthorisationRequest request) {
-        return Optional.ofNullable(consentRestTemplate.postForEntity(remotePisCommonPaymentUrls.createPisAuthorisation(),
-                                                                     request, CreatePisAuthorisationResponse.class, paymentId))
-                   .map(ResponseEntity::getBody);
+        try {
+            return Optional.ofNullable(consentRestTemplate.postForEntity(remotePisCommonPaymentUrls.createPisAuthorisation(),
+                                                                  request, CreatePisAuthorisationResponse.class, paymentId))
+                .map(ResponseEntity::getBody);
+        } catch (CmsRestException cmsRestException) {
+            log.warn("No authorisation was created for the paymentId {}", paymentId);
+            return Optional.empty();
+        }
     }
 
     @Override
