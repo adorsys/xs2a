28d3e56ad4bbe96dccbc62d4a8a9f8faa638bd6b
Konstiantyn Anokhin <kan@adorsys.com.ua>
2019-11-06 09:10:10 +0000

    Koa set version in swagger xs2a connector

---
+++ scripts/set_xs2a_connector_version_in_swagger.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+PSD_VERSION=$(grep -o 'version:.*' xs2a-impl/src/main/resources/static/psd2-*.yaml  | cut -d \" -f2)
+PROJECT_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+sed -i "s/version:.*/version: \"$PSD_VERSION $PROJECT_VERSION Build:$BUILD_NUMBER\"/g"  xs2a-impl/src/main/resources/static/psd2-*.yaml
