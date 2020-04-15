9f62fd0b67e67b350197322dd067170379dd345a
Andrii Murashkin <amu@adorsys.com.ua>
2020-04-15 14:58:18 +0000

    INTEG: Fix postman tests in OAUTH

--- consent-management/cms-integration-test-base/src/main/resources/db/scripts/clear-data.sql
+++ consent-management/cms-integration-test-base/src/main/resources/db/scripts/clear-data.sql
@@ -1,5 +1,5 @@
-DELETE FROM ais_aspsp_account_access;
-DELETE FROM ais_consent_psu_data;
+DELETE FROM aspsp_account_access;
+DELETE FROM consent_psu_data;
 DELETE FROM consent;
 DELETE FROM consent_tpp_information;
 
