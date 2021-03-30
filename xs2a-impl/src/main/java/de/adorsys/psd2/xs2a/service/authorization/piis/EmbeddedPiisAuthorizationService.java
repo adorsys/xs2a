e36a8ee7e816f7ea7e71fba7f1b96f6f7acb26d3
kyaex <kya@adorsys.com.ua>
2021-03-30 12:41:57 +0300

    #1484 Remove duplicates for PIIS consent embedded and decoupled authorisation services

--- xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/authorization/piis/EmbeddedPiisAuthorizationService.java
+++ xs2a-impl/src/main/java/de/adorsys/psd2/xs2a/service/authorization/piis/EmbeddedPiisAuthorizationService.java
@@ -16,74 +16,22 @@
 
 package de.adorsys.psd2.xs2a.service.authorization.piis;
 
-import de.adorsys.psd2.core.data.piis.v1.PiisConsent;
-import de.adorsys.psd2.xs2a.core.authorisation.Authorisation;
 import de.adorsys.psd2.xs2a.core.profile.ScaApproach;
-import de.adorsys.psd2.xs2a.core.psu.PsuIdData;
-import de.adorsys.psd2.xs2a.core.sca.ScaStatus;
-import de.adorsys.psd2.xs2a.domain.authorisation.UpdateAuthorisationRequest;
-import de.adorsys.psd2.xs2a.domain.consent.CreateConsentAuthorizationResponse;
 import de.adorsys.psd2.xs2a.service.authorization.Xs2aAuthorisationService;
-import de.adorsys.psd2.xs2a.service.authorization.processor.model.AuthorisationProcessorResponse;
 import de.adorsys.psd2.xs2a.service.consent.Xs2aConsentService;
 import de.adorsys.psd2.xs2a.service.consent.Xs2aPiisConsentService;
 import de.adorsys.psd2.xs2a.service.mapper.ConsentPsuDataMapper;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @Slf4j
-@RequiredArgsConstructor
-public class EmbeddedPiisAuthorizationService implements PiisAuthorizationService {
-    private final Xs2aConsentService consentService;
-    private final Xs2aPiisConsentService piisConsentService;
-    private final Xs2aAuthorisationService authorisationService;
-    private final ConsentPsuDataMapper consentPsuDataMapper;
-
-    @Override
-    public Optional<CreateConsentAuthorizationResponse> createConsentAuthorization(PsuIdData psuData, String consentId) {
-        Optional<PiisConsent> piisConsentOptional = piisConsentService.getPiisConsentById(consentId);
-        if (piisConsentOptional.isEmpty()) {
-            log.info("Consent-ID [{}]. Create consent authorisation has failed. Consent not found by id.", consentId);
-            return Optional.empty();
-        }
-
-        return consentService.createConsentAuthorisation(consentId, ScaStatus.RECEIVED, psuData)
-                   .map(auth -> {
-                       CreateConsentAuthorizationResponse resp = new CreateConsentAuthorizationResponse();
-
-                       resp.setConsentId(consentId);
-                       resp.setAuthorisationId(auth.getAuthorizationId());
-                       resp.setScaStatus(auth.getScaStatus());
-                       resp.setPsuIdData(psuData);
-
-                       return resp;
-                   });
-    }
-
-    @Override
-    public AuthorisationProcessorResponse updateConsentPsuData(UpdateAuthorisationRequest request, AuthorisationProcessorResponse response) {
-        if (response.hasError()) {
-            log.info("Consent-ID [{}], Authorisation-ID [{}]. Update consent authorisation failed. Error msg: {}.",
-                     request.getBusinessObjectId(), request.getAuthorisationId(), response.getErrorHolder());
-        } else {
-            piisConsentService.updateConsentAuthorisation(consentPsuDataMapper.mapToUpdateConsentPsuDataReq(request, response));
-        }
-
-        return response;
-    }
-
-    @Override
-    public Optional<Authorisation> getConsentAuthorizationById(String authorizationId) {
-        return authorisationService.getAuthorisationById(authorizationId);
-    }
-
-    @Override
-    public Optional<ScaStatus> getAuthorisationScaStatus(String consentId, String authorisationId) {
-        return consentService.getAuthorisationScaStatus(consentId, authorisationId);
+public class EmbeddedPiisAuthorizationService extends AbstractPiisAuthorizationService {
+    public EmbeddedPiisAuthorizationService(Xs2aPiisConsentService piisConsentService,
+                                            Xs2aConsentService consentService,
+                                            Xs2aAuthorisationService authorisationService,
+                                            ConsentPsuDataMapper consentPsuDataMapper) {
+        super(piisConsentService, consentService, authorisationService, consentPsuDataMapper);
     }
 
     @Override
