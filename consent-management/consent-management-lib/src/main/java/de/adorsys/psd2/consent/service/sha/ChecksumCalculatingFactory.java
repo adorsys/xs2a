980da1bc306981e7d49ea034aa2d1964919312c3
Hryhorii Hevorkian <hhe@adorsys.com.ua>
2021-08-31 11:34:26 +0300

    hotfix-7.6.9.

--- consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/sha/ChecksumCalculatingFactory.java
+++ consent-management/consent-management-lib/src/main/java/de/adorsys/psd2/consent/service/sha/ChecksumCalculatingFactory.java
@@ -17,9 +17,12 @@
 package de.adorsys.psd2.consent.service.sha;
 
 
-import de.adorsys.psd2.consent.service.sha.v3.AisChecksumCalculatingServiceV3;
+import de.adorsys.psd2.consent.service.sha.impl.AisChecksumCalculatingServiceV3;
+import de.adorsys.psd2.consent.service.sha.impl.AisChecksumCalculatingServiceV4;
+import de.adorsys.psd2.consent.service.sha.impl.AisChecksumCalculatingServiceV5;
 import de.adorsys.psd2.xs2a.core.consent.ConsentType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.keyvalue.MultiKey;
 import org.apache.commons.collections4.map.MultiKeyMap;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,52 +31,62 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.Optional;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ChecksumCalculatingFactory {
-    private MultiKeyMap<MultiKey, ChecksumCalculatingService> services = new MultiKeyMap();
+    private final MultiKeyMap<String, ChecksumCalculatingService> services = new MultiKeyMap<>();
 
     @Autowired
     private AisChecksumCalculatingServiceV3 aisV3;
     @Autowired
-    private NoProcessingChecksumService  noProcessingService;
+    private AisChecksumCalculatingServiceV4 aisV4;
+    @Autowired
+    private AisChecksumCalculatingServiceV5 aisV5;
+    @Autowired
+    private NoProcessingChecksumService noProcessingService;
 
     @PostConstruct
     public void init() {
         // for deprecated services will use noProcessingService
-        services.put(new MultiKey("001", ConsentType.AIS), noProcessingService);
-        services.put(new MultiKey("002", ConsentType.AIS), noProcessingService);
+        services.put(new MultiKey<>("001", ConsentType.AIS.getName()), noProcessingService);
+        services.put(new MultiKey<>("002", ConsentType.AIS.getName()), noProcessingService);
 
-        services.put(new MultiKey(aisV3.getVersion(), ConsentType.AIS), aisV3);
+        services.put(new MultiKey<>(aisV3.getVersion(), ConsentType.AIS.getName()), aisV3);
+        services.put(new MultiKey<>(aisV4.getVersion(), ConsentType.AIS.getName()), aisV4);
+        services.put(new MultiKey<>(aisV5.getVersion(), ConsentType.AIS.getName()), aisV5);
     }
 
-    /** Provides an appropriate checksum calculator by checksum and consent type
+    /**
+     * Provides an appropriate checksum calculator by checksum and consent type
      *
-     * @param checksum Data with calculator version info
+     * @param checksum    Data with calculator version info
      * @param consentType Type of consent
      * @return Optional value  of  checksum calculating service
      */
     public Optional<ChecksumCalculatingService> getServiceByChecksum(byte[] checksum, ConsentType consentType) {
         if (checksum == null) {
+            log.debug("Checksum is NULL");
             return getDefaultService(consentType);
         }
 
         String checksumStr = new String(checksum);
         String[] elements = checksumStr.split(ChecksumConstant.DELIMITER);
 
-        if (elements.length < 1) {
-            return getDefaultService(consentType);
-        }
-
         String versionSting = elements[ChecksumConstant.VERSION_START_POSITION];
+        Optional<ChecksumCalculatingService> checksumCalculatingServiceOptional = Optional.ofNullable(services.get(new MultiKey<>(versionSting, consentType.getName())));
 
-        return Optional.ofNullable(services.get(new MultiKey(versionSting, consentType)));
+        if (checksumCalculatingServiceOptional.isEmpty()) {
+            log.info("Unknown version: [{}] ", versionSting);
+        }
+        return checksumCalculatingServiceOptional;
     }
 
     private Optional<ChecksumCalculatingService> getDefaultService(ConsentType consentType) {
         if (ConsentType.AIS == consentType) {
-            return Optional.of(aisV3);
+            return Optional.of(aisV5);
         }
+        log.info("Given consent type `[{}]` is not supported.", consentType);
         return Optional.empty();
     }
 }
