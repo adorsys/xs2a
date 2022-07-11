c7cad49854dae11e2d93a6a05280d23ce02ea852
Konstiantyn Anokhin <kan@adorsys.com.ua>
2022-07-11 16:31:52 +0200

    Koa update dockerfile image

--- performance-test/entrypoint.sh
+++ performance-test/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+
 export PATH=$PATH:${JMETER_BIN}
 
 current_time=$(date "+%Y-%m-%d_%H-%M")
@@ -11,14 +13,14 @@ jmeter -n -t test/${JMETER_TEST_FILE} \
 		-Jloop=${JMETER_LOOP} \
 		-Jhost=${XS2A_URL} \
 		-Jprotocol=${XS2A_PROTOCOL} \
-		-j test/reports/Logs.jtl -LINFO -Ljmeter.engine=INFO \
+		-j test/reports/Logs.jtl \
+		-L${DEBUG_LEVEL} \
+		-Ljmeter.engine=${DEBUG_LEVEL} \
 		-Jinfluxdburl=${INFLUX_DB_URL} \
 		-Jinfluxdbport=${INFLUX_DB_PORT} \
 	  -Jinfluxdbdatabasename=${INFLUX_DB_DATABASE_NAME} \
 		-Jinfluxdbusername=${INFLUX_DB_USERNAME} \
 		-Jinfluxdbpassword=${INFLUX_DB_PASSWORD} \
 		-Jinfluxreportname=${INFLUX_REPORT_NAME} \
-		-Jinfluxtestname=${JMETER_TEST_NAME}
-
-
-
+		-Jinfluxtestname=${JMETER_TEST_NAME} \
+		&& tail -f /dev/null
