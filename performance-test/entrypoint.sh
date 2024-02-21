30f189adc12d89d62006255dbf8e1aa50fae1c6c
NdSaid <ndoum0701@gmail.com>
2024-02-21 12:03:43 +0100

    #1868 update copyright

--- performance-test/entrypoint.sh
+++ performance-test/entrypoint.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+#
+# Copyright 2018-2024 adorsys GmbH & Co KG
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or (at
+# your option) any later version. This program is distributed in the hope that
+# it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see https://www.gnu.org/licenses/.
+#
+# This project is also available under a separate commercial license. You can
+# contact us at sales@adorsys.com.
+#
+
 set -e
 
 export PATH=$PATH:${JMETER_BIN}
